76371e0fc3d7b12606ba588a19f51000990f0cd8
Jack Stenglein <jackstenglein@gmail.com>
2022-11-02 15:18:43 -0500

    Add calendar filters

--- frontend/src/calendar/CalendarPage.tsx
+++ frontend/src/calendar/CalendarPage.tsx
@@ -1,14 +1,15 @@
 import { useEffect } from 'react';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import { Scheduler } from '@aldabil/react-scheduler';
 import { ProcessedEvent } from '@aldabil/react-scheduler/types';
 
 import { useApi } from '../api/Api';
 import AvailabilityEditor from './AvailabilityEditor';
 import { Availability, getDisplayString } from '../database/availability';
-import { RequestSnackbar, RequestStatus, useRequest } from '../api/Request';
-import AvailabilityViewer from './AvailabilityViewer';
+import { RequestSnackbar, useRequest } from '../api/Request';
 import { Meeting } from '../database/meeting';
+import { CalendarFilters, useFilters } from './CalendarFilters';
+import ProcessedEventViewer from './ProcessedEventViewer';
 
 const ONE_HOUR = 3600000;
 
@@ -20,6 +21,8 @@ export default function CalendarPage() {
     const ownedAvailabilitiesRequest = useRequest<Availability[]>();
     const otherAvailabilitiesRequest = useRequest<Availability[]>();
     const meetingsRequest = useRequest<Meeting[]>();
+
+    const filters = useFilters();
 
     useEffect(() => {
         if (!ownedAvailabilitiesRequest.isSent()) {
@@ -88,6 +91,7 @@ export default function CalendarPage() {
         }
 
         try {
+            console.log('Deleting availability');
             deleteRequest.onStart();
             await api.deleteAvailability(availability);
             ownedAvailabilitiesRequest.onSuccess(
@@ -101,84 +105,111 @@ export default function CalendarPage() {
         }
     };
 
-    const ownedAvailabilityEvents: ProcessedEvent[] =
-        ownedAvailabilitiesRequest.data?.map((a) => ({
-            event_id: a.id,
-            title: 'Available',
-            start: new Date(a.startTime),
-            end: new Date(a.endTime),
-            availability: a,
-            draggable: false,
-            isOwner: true,
-        })) ?? [];
+    const events: ProcessedEvent[] = [];
 
-    const meetingEvents: ProcessedEvent[] =
-        meetingsRequest.data?.map((m) => ({
-            event_id: m.id,
-            title: getDisplayString(m.type),
-            start: new Date(m.startTime),
-            end: new Date(new Date(m.startTime).getTime() + ONE_HOUR),
-            meeting: m,
-            editable: false,
-            deletable: false,
-            draggable: false,
-        })) ?? [];
+    if (filters.availabilities) {
+        const ownedAvailabilityEvents: ProcessedEvent[] =
+            ownedAvailabilitiesRequest.data?.map((a) => ({
+                event_id: a.id,
+                title: 'Available',
+                start: new Date(a.startTime),
+                end: new Date(a.endTime),
+                availability: a,
+                draggable: false,
+                isOwner: true,
+            })) ?? [];
+        events.push(...ownedAvailabilityEvents);
+    }
+
+    if (filters.meetings) {
+        const meetingEvents: ProcessedEvent[] =
+            meetingsRequest.data?.map((m) => ({
+                event_id: m.id,
+                title: getDisplayString(m.type),
+                start: new Date(m.startTime),
+                end: new Date(new Date(m.startTime).getTime() + ONE_HOUR),
+                meeting: m,
+                editable: false,
+                deletable: false,
+                draggable: false,
+            })) ?? [];
+        events.push(...meetingEvents);
+    }
+
+    console.log('Other availabilities pre filter: ', otherAvailabilitiesRequest.data);
 
     const otherAvailabilityEvents: ProcessedEvent[] =
-        otherAvailabilitiesRequest.data?.map((a) => ({
-            event_id: a.id,
-            title: 'Bookable',
-            start: new Date(a.startTime),
-            end: new Date(a.endTime),
-            availability: a,
-            color: 'red',
-            editable: false,
-            deletable: false,
-            draggable: false,
-            isOwner: false,
-        })) ?? [];
+        otherAvailabilitiesRequest.data
+            ?.filter((a) => {
+                if (!filters.allTypes && a.types.every((t) => !filters.types[t])) {
+                    return false;
+                }
+                if (!filters.allCohorts && !filters.cohorts[a.ownerCohort]) {
+                    return false;
+                }
+                return true;
+            })
+            .map((a) => ({
+                event_id: a.id,
+                title: 'Bookable',
+                start: new Date(a.startTime),
+                end: new Date(a.endTime),
+                availability: a,
+                color: '#d32f2f',
+                editable: false,
+                deletable: false,
+                draggable: false,
+                isOwner: false,
+            })) ?? [];
 
-    const events: ProcessedEvent[] = ownedAvailabilityEvents.concat(
-        otherAvailabilityEvents,
-        meetingEvents
-    );
+    events.push(...otherAvailabilityEvents);
 
     console.log('Full Events: ', events);
 
     return (
-        <Container sx={{ py: 3 }}>
+        <Container sx={{ py: 3 }} maxWidth='xl'>
             <RequestSnackbar request={otherAvailabilitiesRequest} />
             <RequestSnackbar request={deleteRequest} showSuccess />
 
-            <Scheduler
-                view='week'
-                month={{
-                    weekDays: [0, 1, 2, 3, 4, 5, 6],
-                    weekStartOn: 0,
-                    startHour: 0,
-                    endHour: 23,
-                }}
-                week={{
-                    weekDays: [0, 1, 2, 3, 4, 5, 6],
-                    weekStartOn: 0,
-                    startHour: 0,
-                    endHour: 23,
-                    step: 60,
-                }}
-                day={{
-                    startHour: 0,
-                    endHour: 23,
-                    step: 60,
-                }}
-                customEditor={(scheduler) => (
-                    <AvailabilityEditor scheduler={scheduler} onConfirm={onConfirm} />
-                )}
-                onDelete={deleteAvailability}
-                events={events}
-                viewerExtraComponent={(fields, event) => (
-                    <AvailabilityViewer event={event} />
-                )}
-            />
+            <Grid container spacing={2}>
+                <Grid item xs={2.5}>
+                    <CalendarFilters filters={filters} />
+                </Grid>
+                <Grid item xs={9.5}>
+                    <Scheduler
+                        view='week'
+                        month={{
+                            weekDays: [0, 1, 2, 3, 4, 5, 6],
+                            weekStartOn: 0,
+                            startHour: 0,
+                            endHour: 23,
+                        }}
+                        week={{
+                            weekDays: [0, 1, 2, 3, 4, 5, 6],
+                            weekStartOn: 0,
+                            startHour: 0,
+                            endHour: 23,
+                            step: 60,
+                        }}
+                        day={{
+                            startHour: 0,
+                            endHour: 23,
+                            step: 60,
+                        }}
+                        customEditor={(scheduler) => (
+                            <AvailabilityEditor
+                                scheduler={scheduler}
+                                onConfirm={onConfirm}
+                            />
+                        )}
+                        onDelete={deleteAvailability}
+                        events={events}
+                        viewerExtraComponent={(fields, event) => (
+                            <ProcessedEventViewer event={event} />
+                        )}
+                    />
+                </Grid>
+            </Grid>
         </Container>
     );
 }
