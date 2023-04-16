d05d350e160cefbe3c0197eb5d1635c02dacc1b2
Jack Stenglein <jackstenglein@gmail.com>
2023-04-16 15:27:02 -0500

    Improve RequirementDisplay UI on mobile

--- frontend/src/requirements/RequirementDisplay.tsx
+++ frontend/src/requirements/RequirementDisplay.tsx
@@ -53,7 +53,13 @@ const RequirementDisplay: React.FC<RequirementDisplayProps> = ({
     return (
         <>
             <Stack spacing={3}>
-                <Stack direction='row' justifyContent='space-between' alignItems='center'>
+                <Stack
+                    direction='row'
+                    justifyContent='space-between'
+                    alignItems='center'
+                    flexWrap='wrap'
+                    rowGap={1}
+                >
                     <Stack>
                         <Typography variant='h4'>{requirementName}</Typography>
                         <Typography variant='h5' color='text.secondary'>
@@ -61,7 +67,7 @@ const RequirementDisplay: React.FC<RequirementDisplayProps> = ({
                         </Typography>
                     </Stack>
                     {!preview && (
-                        <Stack direction='row' spacing={2}>
+                        <Stack direction='row' spacing={2} alignItems='center'>
                             {isComplete && (
                                 <Chip
                                     icon={<CheckIcon />}
@@ -75,7 +81,7 @@ const RequirementDisplay: React.FC<RequirementDisplayProps> = ({
                                     variant='contained'
                                     onClick={() => setShowUpdateDialog(true)}
                                 >
-                                    {isSlider ? 'Update Progress' : 'Mark Complete'}
+                                    {isSlider ? 'Update' : 'Complete'}
                                 </Button>
                             )}
                             {user.isAdmin && (
