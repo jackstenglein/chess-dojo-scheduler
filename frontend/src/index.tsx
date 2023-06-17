08f156655c419ea5ce0b38f51f3904d18862d72e
Jack Stenglein <jackstenglein@gmail.com>
2023-06-17 09:29:58 -0500

    Add base Google Analytics config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+import { ReportCallback } from 'web-vitals';
+
+ReactGA.initialize('G-9VPNTDELD2');
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -11,7 +15,18 @@ root.render(
     </React.StrictMode>
 );
 
+const sendToAnalytics: ReportCallback = ({ id, name, value }) => {
+    ReactGA.event({
+        category: 'Web Vitals',
+        action: name,
+        label: id,
+        value: Math.round(name === 'CLS' ? value * 1000 : value),
+        nonInteraction: true,
+        transport: 'beacon',
+    });
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToAnalytics);
