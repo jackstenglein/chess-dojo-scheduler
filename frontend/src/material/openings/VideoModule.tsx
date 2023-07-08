b6a8a78129d7369851942f65507b08368e987248
Jack Stenglein <jackstenglein@gmail.com>
2023-07-08 15:47:00 -0500

    Improve spacing of multiple videos in openings

--- frontend/src/material/openings/VideoModule.tsx
+++ frontend/src/material/openings/VideoModule.tsx
@@ -1,38 +1,39 @@
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 
 import { ModuleProps } from './Module';
 
 const VideoModule: React.FC<ModuleProps> = ({ module }) => {
-    if (!module.videoUrls) {
+    if (!module.videoUrls || module.videoUrls.length === 0) {
         return null;
     }
 
     return (
-        <>
+        <Stack spacing={2}>
             {module.videoUrls.map((url, idx) => (
-                <Box
-                    sx={{
-                        width: {
-                            xs: 1,
-                            sm: 0.7,
-                            lg: 0.6,
-                        },
-                        mt: 1,
-                        aspectRatio: '1.77',
-                    }}
-                    key={url}
-                >
-                    <iframe
-                        src={url}
-                        title={`Video ${idx + 1}`}
-                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share'
-                        allowFullScreen={true}
-                        style={{ width: '100%', height: '100%' }}
-                        frameBorder={0}
-                    />
-                </Box>
+                <Stack key={idx}>
+                    <Box
+                        sx={{
+                            width: {
+                                xs: 1,
+                                sm: 0.7,
+                                lg: 0.6,
+                            },
+                            mt: 1,
+                            aspectRatio: '1.77',
+                        }}
+                    >
+                        <iframe
+                            src={url}
+                            title={`Video ${idx + 1}`}
+                            allow='accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share'
+                            allowFullScreen={true}
+                            style={{ width: '100%', height: '100%' }}
+                            frameBorder={0}
+                        />
+                    </Box>
+                </Stack>
             ))}
-        </>
+        </Stack>
     );
 };
 
