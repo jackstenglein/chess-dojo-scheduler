b4c7f900ef01bbc1051df030313baf4571438d40
Jack Stenglein <jackstenglein@gmail.com>
2023-08-11 15:30:09 -0500

    Correctly handle yearly/monthly leaderboard updates

--- backend/tournament/leaderboard/update/main.go
+++ backend/tournament/leaderboard/update/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
+	"sort"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jackstenglein/chess-dojo-scheduler/backend/api"
@@ -25,40 +25,69 @@ func Handler(ctx context.Context, request api.Request) (api.Response, error) {
 	log.SetRequestId(request.RequestContext.RequestID)
 	log.Debugf("Request: %#v", request)
 
-	yearlyLeaderboard := database.Leaderboard{}
-	if err := json.Unmarshal([]byte(request.Body), &yearlyLeaderboard); err != nil {
+	leaderboardReq := database.Leaderboard{}
+	if err := json.Unmarshal([]byte(request.Body), &leaderboardReq); err != nil {
 		return api.Failure(funcName, errors.Wrap(400, "Invalid request: unable to unmarshal request body", "", err)), nil
 	}
 
-	log.Debugf("Request leaderboard: %#v", yearlyLeaderboard)
-	monthlyType, yearlyType, err := getLeaderboardTypes(yearlyLeaderboard)
+	log.Debugf("Request leaderboard: %#v", leaderboardReq)
+	tournamentType, err := getTournamentType(leaderboardReq)
 	if err != nil {
 		return api.Failure(funcName, err), nil
 	}
 
-	yearlyLeaderboard.StartsAt = database.CurrentLeaderboard
-	yearlyLeaderboard.Type = database.LeaderboardType(yearlyType)
-
-	monthlyLeaderboard := database.Leaderboard{
-		Type:        database.LeaderboardType(monthlyType),
-		StartsAt:    database.CurrentLeaderboard,
-		TimeControl: yearlyLeaderboard.TimeControl,
-		Players:     yearlyLeaderboard.Players,
-	}
-
-	if err := repository.SetLeaderboard(monthlyLeaderboard); err != nil {
+	currentMonthly, err := repository.GetLeaderboard("monthly", tournamentType, leaderboardReq.TimeControl, database.CurrentLeaderboard)
+	if err != nil {
 		return api.Failure(funcName, err), nil
 	}
-	if err := repository.SetLeaderboard(yearlyLeaderboard); err != nil {
+
+	currentYearly, err := repository.GetLeaderboard("yearly", tournamentType, leaderboardReq.TimeControl, database.CurrentLeaderboard)
+	if err != nil {
+		return api.Failure(funcName, err), nil
+	}
+
+	yearlyPlayers := make(map[string]database.LeaderboardPlayer)
+	for _, player := range currentYearly.Players {
+		yearlyPlayers[player.Username] = player
+	}
+
+	currentMonthlyScores := make(map[string]float32)
+	for _, player := range currentMonthly.Players {
+		currentMonthlyScores[player.Username] = player.Score
+	}
+
+	for _, player := range leaderboardReq.Players {
+		currentScore, _ := currentMonthlyScores[player.Username]
+		if currentPlayer, ok := yearlyPlayers[player.Username]; !ok {
+			yearlyPlayers[player.Username] = player
+		} else {
+			currentPlayer.Score += player.Score - currentScore
+			yearlyPlayers[player.Username] = currentPlayer
+		}
+	}
+
+	yearlyPlayersSlice := make([]database.LeaderboardPlayer, 0, len(yearlyPlayers))
+	for _, v := range yearlyPlayers {
+		yearlyPlayersSlice = append(yearlyPlayersSlice, v)
+	}
+	sort.Slice(yearlyPlayersSlice, func(i, j int) bool {
+		return yearlyPlayersSlice[i].Score > yearlyPlayersSlice[j].Score
+	})
+	currentYearly.Players = yearlyPlayersSlice
+
+	currentMonthly.Players = leaderboardReq.Players
+	if err := repository.SetLeaderboard(*currentMonthly); err != nil {
+		return api.Failure(funcName, err), nil
+	}
+	if err := repository.SetLeaderboard(*currentYearly); err != nil {
 		return api.Failure(funcName, err), nil
 	}
 
 	return api.Success(funcName, nil), nil
 }
 
-// getLeaderboardTypes returns the type names for the monthly leaderboard and the yearly
-// leaderboard, in that order.
-func getLeaderboardTypes(leaderboard database.Leaderboard) (string, string, error) {
+// getTournamentType returns the tournament type for the given leaderboard.
+func getTournamentType(leaderboard database.Leaderboard) (string, error) {
 	tournamentType := ""
 	switch leaderboard.Type {
 	case "Arena Total":
@@ -72,15 +101,8 @@ func getLeaderboardTypes(leaderboard database.Leaderboard) (string, string, erro
 	case "Endgame Sparring Total":
 		tournamentType = "ENDGAME_SPARRING"
 	default:
-		return "", "", errors.New(400, fmt.Sprintf("Invalid request: type `%s` is invalid", leaderboard.Type), "")
+		return "", errors.New(400, fmt.Sprintf("Invalid request: type `%s` is invalid", leaderboard.Type), "")
 	}
 
-	timeControl := strings.ToUpper(leaderboard.TimeControl)
-	if timeControl != "BLITZ" && timeControl != "RAPID" && timeControl != "CLASSICAL" {
-		return "", "", errors.New(400, fmt.Sprintf("Invalid request: timeControl `%s` is invalid", leaderboard.TimeControl), "")
-	}
-
-	return fmt.Sprintf("LEADERBOARD_MONTHLY_%s_%s", tournamentType, timeControl),
-		fmt.Sprintf("LEADERBOARD_YEARLY_%s_%s", tournamentType, timeControl),
-		nil
+	return tournamentType, nil
 }
