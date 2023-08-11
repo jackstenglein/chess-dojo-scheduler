a338c23955c65833ba0e17944791817006eafad6
Jack Stenglein <jackstenglein@gmail.com>
2023-08-11 12:42:26 -0500

    Handle swiss in CreateTournament

--- backend/tournament/create/main.go
+++ backend/tournament/create/main.go
@@ -35,6 +35,23 @@ type LichessArenaResponse struct {
 	} `json:"position"`
 }
 
+type LichessSwissResponse struct {
+	Id       string `json:"id"`
+	StartsAt string `json:"startsAt"`
+	Name     string `json:"name"`
+	Clock    struct {
+		Limit     int `json:"limit"`
+		Increment int `json:"increment"`
+	} `json:"clock"`
+	NumRounds   int    `json:"nbRounds"`
+	Rated       bool   `json:"rated"`
+	Description string `json:"description"`
+	Position    struct {
+		Name string `json:"name"`
+		Fen  string `json:"fen"`
+	} `json:"position"`
+}
+
 const funcName = "tournament-create-handler"
 const lichessArenaPrefix = "https://lichess.org/tournament/"
 const lichessSwissPrefix = "https://lichess.org/swiss/"
@@ -58,19 +75,19 @@ func Handler(ctx context.Context, request api.Request) (api.Response, error) {
 	var tournament *database.Tournament
 	if strings.HasPrefix(req.Url, lichessArenaPrefix) {
 		tournament, err = handleArena(req.Url)
-		if err != nil {
-			return api.Failure(funcName, err), nil
-		}
 	} else if strings.HasPrefix(req.Url, lichessSwissPrefix) {
-
+		tournament, err = handleSwiss(req.Url)
 	} else {
 		return api.Failure(funcName, errors.New(400, "Invalid request: unknown URL format", "")), nil
+	}
+
+	if err != nil {
+		return api.Failure(funcName, err), nil
 	}
 
 	if err := repository.SetTournament(tournament); err != nil {
 		return api.Failure(funcName, err), nil
 	}
-
 	return api.Success(funcName, tournament), nil
 }
 
@@ -111,6 +128,47 @@ func handleArena(url string) (*database.Tournament, error) {
 		Fen:              arena.Position.Fen,
 		Url:              url,
 		LengthMinutes:    arena.LengthMinutes,
+	}
+	return tournament, nil
+}
+
+func handleSwiss(url string) (*database.Tournament, error) {
+	id := strings.TrimPrefix(url, lichessSwissPrefix)
+	if id == "" {
+		return nil, errors.New(400, "Invalid request: Lichess swiss id must not be empty", "")
+	}
+
+	resp, err := http.Get(fmt.Sprintf("https://lichess.org/api/swiss/%s", id))
+	if err != nil {
+		err = errors.Wrap(500, "Temporary server error", "Failed to fetch Lichess swiss tournament", err)
+		return nil, err
+	}
+
+	if resp.StatusCode == 404 {
+		return nil, errors.New(404, fmt.Sprintf("Invalid request: Lichess swiss `%s` not found", id), "")
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New(500, "Temporary server error", fmt.Sprintf("Lichess returned non 200 for swiss `%s`", id))
+	}
+
+	var swiss LichessSwissResponse
+	if err := json.NewDecoder(resp.Body).Decode(&swiss); err != nil {
+		err = errors.Wrap(500, "Temporary server error", "Failed to unmarshal Lichess response", err)
+		return nil, err
+	}
+
+	tournament := &database.Tournament{
+		Type:             database.TournamentType_Swiss,
+		StartsAt:         swiss.StartsAt,
+		Id:               swiss.Id,
+		Name:             swiss.Name,
+		Description:      swiss.Description,
+		Rated:            swiss.Rated,
+		LimitSeconds:     swiss.Clock.Limit,
+		IncrementSeconds: swiss.Clock.Increment,
+		Fen:              swiss.Position.Fen,
+		Url:              url,
+		NumRounds:        swiss.NumRounds,
 	}
 	return tournament, nil
 }
