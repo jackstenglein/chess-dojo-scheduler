9b5c8c5b18ae7e7751d44f558e8a88c43077c07e
Jack Stenglein <jackstenglein@gmail.com>
2023-11-01 16:26:40 -0500

    Setup react native paper, add initial landing page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,21 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Button, PaperProvider, Text } from 'react-native-paper';
+import { router } from 'expo-router';
+
+import Stack from '../components/Stack';
 
 export default function App() {
+    const onSignUp = () => {
+        router.push('/settings/');
+    };
+
+    const onSignIn = () => {
+        router.push('/settings/');
+    };
+
     return (
-        <View style={styles.container}>
-            <Link href='/settings/'>Settings</Link>
-            <StatusBar style='auto' />
-        </View>
+        <PaperProvider>
+            <Stack
+                spacing={4}
+                style={{
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    paddingHorizontal: 8,
+                }}
+            >
+                <Text variant='displayMedium' style={{ textAlign: 'center' }}>
+                    ChessDojo Scoreboard
+                </Text>
+
+                <Text variant='headlineSmall' style={{ textAlign: 'center' }}>
+                    The ChessDojo{' '}
+                    <Text style={{ color: '#F7941F' }}>Training Program</Text> offers
+                    structured training plans for all levels 0-2500, along with an active
+                    and supportive community
+                </Text>
+
+                <Stack direction='row' spacing={3} style={{ justifyContent: 'center' }}>
+                    <Button
+                        mode='contained'
+                        style={{ paddingVertical: 12 }}
+                        labelStyle={{ fontWeight: 'bold' }}
+                        onPress={onSignUp}
+                    >
+                        Sign Up for Free
+                    </Button>
+
+                    <Button
+                        mode='outlined'
+                        style={{ paddingVertical: 12 }}
+                        labelStyle={{ fontWeight: 'bold' }}
+                        onPress={onSignIn}
+                    >
+                        Sign In
+                    </Button>
+                </Stack>
+
+                <StatusBar style='auto' />
+            </Stack>
+        </PaperProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
