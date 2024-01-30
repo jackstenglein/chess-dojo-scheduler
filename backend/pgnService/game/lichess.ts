caa26ffce0697ae76a3d9c8da08075955aea81a0
Jack Stenglein <jackstenglein@gmail.com>
2024-01-30 13:46:55 -0600

    Fix error message when Lichess study is private

--- backend/pgnService/game/lichess.ts
+++ backend/pgnService/game/lichess.ts
@@ -11,8 +11,20 @@ export async function getLichessChapter(url?: string): Promise<string> {
         });
     }
 
-    const response = await axios.get<string>(`${url}.pgn?source=true`);
-    return response.data;
+    try {
+        const response = await axios.get<string>(`${url}.pgn?source=true`);
+        return response.data;
+    } catch (err) {
+        if (err.response?.status === 401) {
+            throw new ApiError({
+                statusCode: 400,
+                publicMessage:
+                    'Invalid request: Lichess study must be public or unlisted',
+                cause: err,
+            });
+        }
+        throw err;
+    }
 }
 
 export async function getLichessStudy(url?: string): Promise<string[]> {
@@ -24,18 +36,30 @@ export async function getLichessStudy(url?: string): Promise<string[]> {
         });
     }
 
-    const response = await axios.get<string>(`${url}.pgn?source=true`);
-    const games = response.data.split('\n\n\n[');
-    return games
-        .map((g, i) => {
-            g = g.trim();
-            if (!g) {
-                return g;
-            }
-            if (i === 0) {
-                return g;
-            }
-            return `[${g}`;
-        })
-        .filter((v) => v !== '');
+    try {
+        const response = await axios.get<string>(`${url}.pgn?source=true`);
+        const games = response.data.split('\n\n\n[');
+        return games
+            .map((g, i) => {
+                g = g.trim();
+                if (!g) {
+                    return g;
+                }
+                if (i === 0) {
+                    return g;
+                }
+                return `[${g}`;
+            })
+            .filter((v) => v !== '');
+    } catch (err) {
+        if (err.response?.status === 401) {
+            throw new ApiError({
+                statusCode: 400,
+                publicMessage:
+                    'Invalid request: Lichess study must be public or unlisted',
+                cause: err,
+            });
+        }
+        throw err;
+    }
 }
