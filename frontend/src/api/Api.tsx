8cbda5ec1665cfd7bd4c12afaa2919d1da9ce6c3
Jack Stenglein <jackstenglein@gmail.com>
2024-03-25 18:05:32 -0500

    Allow collapsing comments

--- frontend/src/api/Api.tsx
+++ frontend/src/api/Api.tsx
@@ -52,6 +52,8 @@ import {
     createComment,
     createGame,
     CreateGameRequest,
+    deleteComment,
+    DeleteCommentRequest,
     deleteGame,
     featureGame,
     GameApiContextType,
@@ -297,6 +299,8 @@ export function ApiProvider({ children }: { children: ReactNode }) {
             ) => createComment(idToken, cohort, id, comment, existingComments),
             updateComment: (update: UpdateCommentRequest) =>
                 updateComment(idToken, update),
+            deleteComment: (request: DeleteCommentRequest) =>
+                deleteComment(idToken, request),
             requestReview: (cohort: string, id: string, reviewType: GameReviewType) =>
                 requestReview(idToken, cohort, id, reviewType),
             markReviewed: (cohort: string, id: string) =>
