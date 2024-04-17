47670b91c64245cc37a73005db05389a63192eb9
Danimal <danimal@hackpoetic.com>
2024-04-17 18:49:15 -0400

    fix: incorrect test name

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders front page of app', async () => {
     render(<App />);
     const el = await screen.findByText(/training program/i);
     expect(el).toBeInTheDocument();
