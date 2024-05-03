ad0590307cca16d6ca8477f5c2eb0bb631b9f0d0
Danimal <danimal@hackpoetic.com>
2024-05-03 13:04:52 -0400

    feat(feat): visual starting position

--- frontend/src/games/import/StartingPositionForm.tsx
+++ frontend/src/games/import/StartingPositionForm.tsx
@@ -1,13 +1,7 @@
+import { Box, Stack } from '@mui/material';
 import { useState } from 'react';
-
-import { MenuItem } from '@mui/base';
-import { Stack, TextField } from '@mui/material';
-import { DatePicker } from '@mui/x-date-pickers';
-
-import { DateTime } from 'luxon';
-
 import { GameSubmissionType, RemoteGame } from '../../api/gameApi';
-import { getGameHeader } from '../edit/SubmitGamePreflight';
+import Board from '../../board/Board';
 import { ImportButton } from './ImportButton';
 
 interface StartingPositionFormProps {
@@ -15,103 +9,30 @@ interface StartingPositionFormProps {
     onSubmit: (game: RemoteGame) => void;
 }
 
+const startingPositionFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
 export const StartingPositionForm: React.FC<StartingPositionFormProps> = ({
     loading,
     onSubmit,
 }) => {
-    const [white, setWhite] = useState('');
-    const [black, setBlack] = useState('');
-    const [date, setDate] = useState<DateTime | null>(null);
-    const [result, setResult] = useState('');
-    const [errors, setErrors] = useState<Record<string, string>>({});
+    const [fen] = useState(startingPositionFen);
 
     const handleSubmit = () => {
-        const newErrors: Record<string, string> = {};
-
-        if (white.trim() === '') {
-            newErrors.white = 'This field is required';
-        }
-
-        if (black.trim() === '') {
-            newErrors.black = 'This field is required';
-        }
-
-        if (!date || !date.isValid) {
-            newErrors.date = 'This field is required';
-        }
-
-        if (result.trim() === '') {
-            newErrors.result = 'This field is required';
-        }
-        setErrors(newErrors);
-        if (Object.entries(newErrors).length > 0) {
-            return;
-        }
-
         onSubmit({
             type: GameSubmissionType.StartingPosition,
-            headers: [getGameHeader({ white, black, date, result })],
         });
     };
 
     return (
-        <Stack alignItems='flex-end' spacing={2}>
-            <Stack
-                direction='row'
-                spacing={1}
-                alignItems='baseline'
-                justifyContent='space-between'
+        <Stack>
+            <Box
+                sx={{
+                    aspectRatio: 1,
+                    width: 'var(--board-size)',
+                }}
             >
-                <TextField
-                    fullWidth
-                    data-cy={`white`}
-                    label='White'
-                    value={white}
-                    onChange={(e) => setWhite(e.target.value)}
-                    error={!!errors.white}
-                    helperText={errors.white}
-                />
-
-                <TextField
-                    fullWidth
-                    data-cy={`black`}
-                    label='Black'
-                    value={black}
-                    onChange={(e) => setBlack(e.target.value)}
-                    error={!!errors.black}
-                    helperText={errors.black}
-                />
-
-                <TextField
-                    fullWidth
-                    select
-                    data-cy={`result`}
-                    label='Result'
-                    value={result}
-                    onChange={(e) => setResult(e.target.value)}
-                    error={!!errors.result}
-                    helperText={errors.result}
-                >
-                    <MenuItem value='1-0'>White Won</MenuItem>
-                    <MenuItem value='1/2-1/2'>Draw</MenuItem>
-                    <MenuItem value='0-1'>Black Won</MenuItem>
-                </TextField>
-
-                <DatePicker
-                    label='Date'
-                    disableFuture
-                    value={date}
-                    onChange={(newValue) => setDate(newValue)}
-                    slotProps={{
-                        textField: {
-                            id: `date`,
-                            error: !!errors.date,
-                            helperText: errors.date,
-                            fullWidth: true,
-                        },
-                    }}
-                />
-            </Stack>
+                <Board config={{ fen: fen }} />
+            </Box>
             <ImportButton loading={loading} onClick={handleSubmit} />
         </Stack>
     );
