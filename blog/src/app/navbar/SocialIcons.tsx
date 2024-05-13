c743a0a8fb51200bd3e1f6f3564d519fae93deb7
Jack Stenglein <jackstenglein@gmail.com>
2024-05-13 18:00:57 -0500

    fix(blog): component display name error

--- blog/src/app/navbar/SocialIcons.tsx
+++ blog/src/app/navbar/SocialIcons.tsx
@@ -7,46 +7,8 @@ import {
     faTwitch,
 } from '@fortawesome/free-brands-svg-icons';
 import { FacebookOutlined, Instagram, Twitter, YouTube } from '@mui/icons-material';
-import { IconButton, Stack, SvgIcon, Tooltip, useMediaQuery } from '@mui/material';
-import { forwardRef } from 'react';
-
-type FontAwesomeSvgIconProps = {
-    icon: any;
-};
-
-export const FontAwesomeSvgIcon = forwardRef<SVGSVGElement, FontAwesomeSvgIconProps>(
-    (props, ref) => {
-        const { icon } = props;
-
-        const {
-            icon: [width, height, , , svgPathData],
-        } = icon;
-
-        return (
-            <SvgIcon
-                ref={ref}
-                viewBox={`0 0 ${width} ${height}`}
-                fontSize='small'
-                className='MuiChip-icon'
-            >
-                {typeof svgPathData === 'string' ? (
-                    <path d={svgPathData} />
-                ) : (
-                    /**
-                     * A multi-path Font Awesome icon seems to imply a duotune icon. The 0th path seems to
-                     * be the faded element (referred to as the "secondary" path in the Font Awesome docs)
-                     * of a duotone icon. 40% is the default opacity.
-                     *
-                     * @see https://fontawesome.com/how-to-use/on-the-web/styling/duotone-icons#changing-opacity
-                     */
-                    svgPathData.map((d: string, i: number) => (
-                        <path key={i} style={{ opacity: i === 0 ? 0.4 : 1 }} d={d} />
-                    ))
-                )}
-            </SvgIcon>
-        );
-    },
-);
+import { IconButton, Stack, Tooltip, useMediaQuery } from '@mui/material';
+import { FontAwesomeSvgIcon } from './PawnIcon';
 
 export function DiscordIcon() {
     return <FontAwesomeSvgIcon icon={faDiscord} />;
