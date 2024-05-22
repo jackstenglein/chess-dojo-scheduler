5efb79c7a3d6aa4480ae3bd914cce0c57d8b711d
Danimal <danimal@hackpoetic.com>
2024-05-21 20:14:10 -0400

    fix(frontend test): import tests failing soon to be new lint rules

--- frontend/src/games/import/OnlineGameForm.test.tsx
+++ frontend/src/games/import/OnlineGameForm.test.tsx
@@ -2,8 +2,10 @@ import { render } from '@testing-library/react';
 import { expect, it } from 'vitest';
 import { OnlineGameForm } from './OnlineGameForm';
 
-it('renders import button', async () => {
-    const { getByRole } = render(<OnlineGameForm loading={false} onSubmit={() => {}} />);
+it('renders import button', () => {
+    const { getByRole } = render(
+        <OnlineGameForm loading={false} onClose={() => ({})} onSubmit={() => ({})} />,
+    );
 
     expect(getByRole('button', { name: /import/i })).toBeDefined();
 });
--- frontend/src/games/import/PGNForm.test.tsx
+++ frontend/src/games/import/PGNForm.test.tsx
@@ -2,8 +2,10 @@ import { render } from '@testing-library/react';
 import { expect, it } from 'vitest';
 import { PGNForm } from './PGNForm';
 
-it('renders import button', async () => {
-    const { getByRole } = render(<PGNForm loading={false} onSubmit={() => {}} />);
+it('renders import button', () => {
+    const { getByRole } = render(
+        <PGNForm loading={false} onClose={() => ({})} onSubmit={() => ({})} />,
+    );
 
     expect(getByRole('button', { name: /import/i })).toBeDefined();
 });
