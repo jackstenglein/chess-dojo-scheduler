610405dc2a7c253daba7aee9e11627766f25c3f6
Jack Stenglein <jackstenglein@gmail.com>
2024-06-05 17:57:49 -0500

    fix(blog): issues with DojoTalks post

--- blog/src/app/blog/items.ts
+++ blog/src/app/blog/items.ts
@@ -1,7 +1,7 @@
 import { StaticImageData } from 'next/image';
 import dojoDigestVol8Image from './dojo-digest/vol-8/dojo_3-0.webp';
+import talkstop2025 from './dojo-talks/top-10-2025/image.webp';
 import tacticsTestImage from './tactics-test/image.png';
-import talkstop2025 from './dojo-talks-Who Will Be Top 10 in 2025/Who Will Be Top 10 in 2025.webp'
 
 export interface BlogItem {
     title: string;
@@ -19,7 +19,7 @@ const items: BlogItem[] = [
         title: 'Who Will Be Top 10 in 2025? | Dojo Talks',
         subtitle: 'Jesse, Kostya, and David • June 5, 2024',
         body: `GM Jesse Kraai, IM David Pruess, and IM Kostya Kavutskiy forecast who will be the top 10 FIDE rated chess players in June 2025 in today's episode of Dojo Talks, the ChessDojo podcast.`,
-        href: '/blog/dojo-talks-top-10-2025',
+        href: '/blog/dojo-talks/top-10-2025',
         image: {
             src: talkstop2025,
             alt: '',
