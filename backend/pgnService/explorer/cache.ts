cd3736fc995ccd9dce504df7adca091bc5596e17
Jack Stenglein <jackstenglein@gmail.com>
2024-07-03 17:58:04 -0500

    use mongodb instead of local dynamo

--- backend/pgnService/explorer/cache.ts
+++ backend/pgnService/explorer/cache.ts
@@ -1,91 +1,98 @@
-import {
-    BatchWriteItemCommand,
-    DynamoDBClient,
-    GetItemCommand,
-} from '@aws-sdk/client-dynamodb';
-import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { Collection, MongoClient } from 'mongodb';
 import { ExplorerPosition } from './types';
 
-const dynamo = new DynamoDBClient({
-    region: 'us-east-1',
-    endpoint: 'http://localhost:8080',
-});
-const mastersTable = 'local-masters';
+// const dynamo = new DynamoDBClient({
+//     region: 'us-east-1',
+//     endpoint: 'http://localhost:8000',
+// });
+// const mastersTable = 'local-masters';
 
-const cache = new Map<string, ExplorerPosition>();
+const client = new MongoClient('mongodb://localhost:27017');
+let collection: Collection<ExplorerPosition> | null = null;
 
-const MAX_CACHE_SIZE = 2_000_000;
+export async function initialize() {
+    await client.connect();
+    const db = client.db('masters');
+    collection = db.collection<ExplorerPosition>('documents');
+    const result = await collection.createIndex({ normalizedFen: 1 });
+    console.log(`${new Date().toISOString()} INFO: created index: ${result}`);
+}
+
+export function close() {
+    return client.close();
+}
 
 export async function set(position: ExplorerPosition) {
-    cache.set(position.normalizedFen, position);
-
-    if (cache.size > MAX_CACHE_SIZE) {
-        await writeToDynamo();
-    }
+    await collection!.replaceOne({ normalizedFen: position.normalizedFen }, position, {
+        upsert: true,
+    });
 }
 
 export async function get(fen: string): Promise<ExplorerPosition | undefined> {
-    if (cache.has(fen)) {
-        return cache.get(fen)!;
-    }
+    const position = await collection!.findOne({ normalizedFen: fen });
+    return (position as ExplorerPosition) || null;
 
-    const input = new GetItemCommand({
-        Key: {
-            normalizedFen: {
-                S: fen,
-            },
-            id: {
-                S: 'POSITION',
-            },
-        },
-        TableName: mastersTable,
-    });
+    // if (cache.has(fen)) {
+    //     return cache.get(fen)!;
+    // }
 
-    const output = await dynamo.send(input);
-    if (!output.Item) {
-        return undefined;
-    }
+    // const input = new GetItemCommand({
+    //     Key: {
+    //         normalizedFen: {
+    //             S: fen,
+    //         },
+    //         id: {
+    //             S: 'POSITION',
+    //         },
+    //     },
+    //     TableName: mastersTable,
+    // });
 
-    return unmarshall(output.Item) as ExplorerPosition;
+    // const output = await dynamo.send(input);
+    // if (!output.Item) {
+    //     return undefined;
+    // }
+
+    // return unmarshall(output.Item) as ExplorerPosition;
 }
 
-async function writeToDynamo() {
-    console.log(`${new Date().toISOString()} INFO: Writing cache to Dynamo`);
+// async function writeToDynamo() {
+//     console.log(`${new Date().toISOString()} INFO: Writing cache to Dynamo`);
 
-    let entries: ExplorerPosition[] = [];
-    for (const entry of cache) {
-        entries.push(entry[1]);
+//     let entries: ExplorerPosition[] = [];
+//     for (const entry of cache) {
+//         entries.push(entry[1]);
 
-        if (entries.length === 25) {
-            await dynamo.send(
-                new BatchWriteItemCommand({
-                    RequestItems: {
-                        [mastersTable]: entries.map((e) => ({
-                            PutRequest: {
-                                Item: marshall(e),
-                            },
-                        })),
-                    },
-                }),
-            );
-            entries = [];
-        }
-    }
+//         if (entries.length === 25) {
+//             await dynamo.send(
+//                 new BatchWriteItemCommand({
+//                     RequestItems: {
+//                         [mastersTable]: entries.map((e) => ({
+//                             PutRequest: {
+//                                 Item: marshall(e),
+//                             },
+//                         })),
+//                     },
+//                 }),
+//             );
+//             entries = [];
+//         }
+//     }
 
-    if (entries.length > 0) {
-        await dynamo.send(
-            new BatchWriteItemCommand({
-                RequestItems: {
-                    [mastersTable]: entries.map((e) => ({
-                        PutRequest: {
-                            Item: marshall(e),
-                        },
-                    })),
-                },
-            }),
-        );
-        entries = [];
-    }
+//     if (entries.length > 0) {
+//         await dynamo.send(
+//             new BatchWriteItemCommand({
+//                 RequestItems: {
+//                     [mastersTable]: entries.map((e) => ({
+//                         PutRequest: {
+//                             Item: marshall(e),
+//                         },
+//                     })),
+//                 },
+//             }),
+//         );
+//         entries = [];
+//     }
 
-    cache.clear();
-}
+//     cache.clear();
+// }
