6d35e5cd137e8ab4d6131988b419737fb9a3ac77
Autumn Green <chao-mu@hackpoetic.com>
2024-07-12 19:36:03 -0400

    refactor(frontend): pull config from .env files (#627)
    * feat(frontend): externalize config from source
    
    * fix(frontend): update package-lock.json
    
    * fix: remove chat from .env files
    
    * fix(frontend): config loading
    
    ---------
    
    Co-authored-by: chao-mu <me@queerlyautumn.com>
    Co-authored-by: Jack Stenglein <jackstenglein@gmail.com>

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,32 @@
+import { z } from 'zod';
+
+export const EnvSchema = z.object({
+    auth: z.object({
+        region: z.literal('us-east-1'),
+        userPoolId: z.string(),
+        userPoolWebClientId: z.string(),
+        oauth: z.object({
+            domain: z.string(),
+            scope: z
+                .string()
+                .transform((value) => value.split(',').map((v) => v.trim()))
+                .pipe(z.string().array()),
+            redirectSignIn: z.string(),
+            redirectSignOut: z.string(),
+            responseType: z.literal('code'),
+        }),
+    }),
+    api: z.object({
+        baseUrl: z.string(),
+    }),
+    media: z.object({
+        picturesBucket: z.string(),
+    }),
+    stripe: z.object({
+        publishableKey: z.string(),
+    }),
+});
+
 export interface Config {
     auth: {
         region: 'us-east-1';
@@ -22,120 +51,28 @@ export interface Config {
     };
 }
 
-enum ConfigName {
-    Test = 'test',
-    Development = 'development',
-    Beta = 'beta',
-    Prod = 'production',
-}
-
-const config: Record<ConfigName, Config> = {
-    test: {
-        auth: {
-            region: 'us-east-1',
-            userPoolId: 'us-east-1_apywr6kwu',
-            userPoolWebClientId: '76en8knncv8bfpfgbheua6j6k',
-            oauth: {
-                domain: 'authdev.chessdojo.club',
-                scope: ['profile', 'email', 'openid'],
-                redirectSignIn: 'http://localhost:3000',
-                redirectSignOut: 'http://localhost:3000',
-                responseType: 'code',
-            },
-        },
-        api: {
-            baseUrl: 'https://c2qamdaw08.execute-api.us-east-1.amazonaws.com',
-        },
-        media: {
-            picturesBucket: 'https://chess-dojo-dev-pictures.s3.amazonaws.com',
-        },
-        stripe: {
-            publishableKey:
-                'pk_test_51OB6imGilmvijaecMJqdvLJdu89BcghnjU7eOIoCwlBl8DeV6i2XojJOaZ36lamZMuVjO7aorXtl90OcdtAstFfF0022uf0sdp',
-        },
-    },
-
-    development: {
-        auth: {
-            region: 'us-east-1',
-            userPoolId: 'us-east-1_apywr6kwu',
-            userPoolWebClientId: '76en8knncv8bfpfgbheua6j6k',
-            oauth: {
-                domain: 'authdev.chessdojo.club',
-                scope: ['profile', 'email', 'openid'],
-                redirectSignIn: 'http://localhost:3000',
-                redirectSignOut: 'http://localhost:3000',
-                responseType: 'code',
-            },
-        },
-        api: {
-            baseUrl: 'https://c2qamdaw08.execute-api.us-east-1.amazonaws.com',
-        },
-        media: {
-            picturesBucket: 'https://chess-dojo-dev-pictures.s3.amazonaws.com',
-        },
-        stripe: {
-            publishableKey:
-                'pk_test_51OB6imGilmvijaecMJqdvLJdu89BcghnjU7eOIoCwlBl8DeV6i2XojJOaZ36lamZMuVjO7aorXtl90OcdtAstFfF0022uf0sdp',
-        },
-    },
-
-    beta: {
-        auth: {
-            region: 'us-east-1',
-            userPoolId: 'us-east-1_0revSxCzf',
-            userPoolWebClientId: '1dfi5rar7a2fr5samugigrmise',
-            oauth: {
-                domain: 'auth.chessdojo.club',
-                scope: ['profile', 'email', 'openid'],
-                redirectSignIn: 'https://beta.chessdojo.club',
-                redirectSignOut: 'https://beta.chessdojo.club',
-                responseType: 'code',
-            },
-        },
-        api: {
-            baseUrl: 'https://g4shdaq6ug.execute-api.us-east-1.amazonaws.com',
-        },
-        media: {
-            picturesBucket: 'https://chess-dojo-prod-pictures.s3.amazonaws.com',
-        },
-        stripe: {
-            publishableKey:
-                'pk_live_51OB6imGilmvijaecicnOhS1rqgX6VofcmTgi4n3TdhYoPgutx4W8HnUch6iQE7GL62fngez6mL471YWiZSrUhbJI007MlHx5CM',
-        },
-    },
-
-    production: {
-        auth: {
-            region: 'us-east-1',
-            userPoolId: 'us-east-1_0revSxCzf',
-            userPoolWebClientId: '1dfi5rar7a2fr5samugigrmise',
-            oauth: {
-                domain: 'auth.chessdojo.club',
-                scope: ['profile', 'email', 'openid'],
-                redirectSignIn: 'https://www.chessdojo.club',
-                redirectSignOut: 'https://www.chessdojo.club',
-                responseType: 'code',
-            },
-        },
-        api: {
-            baseUrl: 'https://g4shdaq6ug.execute-api.us-east-1.amazonaws.com',
-        },
-        media: {
-            picturesBucket: 'https://chess-dojo-prod-pictures.s3.amazonaws.com',
-        },
-        stripe: {
-            publishableKey:
-                'pk_live_51OB6imGilmvijaecicnOhS1rqgX6VofcmTgi4n3TdhYoPgutx4W8HnUch6iQE7GL62fngez6mL471YWiZSrUhbJI007MlHx5CM',
-        },
-    },
-};
-
 export function getConfig(): Config {
-    const envOverride = process.env.NEXT_PUBLIC_BUILD_NODE_ENV;
-    let env: ConfigName = process.env.NODE_ENV as ConfigName;
-    if (envOverride && Object.values(ConfigName).includes(envOverride as ConfigName)) {
-        env = envOverride as ConfigName;
-    }
-    return config[env];
+    return EnvSchema.parse({
+        auth: {
+            region: process.env.NEXT_PUBLIC_AUTH_REGION,
+            userPoolId: process.env.NEXT_PUBLIC_AUTH_USER_POOL_ID,
+            userPoolWebClientId: process.env.NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID,
+            oauth: {
+                domain: process.env.NEXT_PUBLIC_AUTH_OAUTH_DOMAIN,
+                scope: process.env.NEXT_PUBLIC_AUTH_OAUTH_SCOPES,
+                redirectSignIn: process.env.NEXT_PUBLIC_AUTH_OAUTH_REDIRECT_SIGN_IN,
+                redirectSignOut: process.env.NEXT_PUBLIC_AUTH_OAUTH_REDIRECT_SIGN_OUT,
+                responseType: process.env.NEXT_PUBLIC_AUTH_OAUTH_RESPONSE_TYPE,
+            },
+        },
+        api: {
+            baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+        },
+        media: {
+            picturesBucket: process.env.NEXT_PUBLIC_MEDIA_PICTURES_BUCKET,
+        },
+        stripe: {
+            publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+        },
+    });
 }
