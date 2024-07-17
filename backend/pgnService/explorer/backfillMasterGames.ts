0204bb2ebce0099050feb066f4ffa971cb4875f2
Jack Stenglein <jackstenglein@gmail.com>
2024-07-17 18:05:40 -0500

    backfill new TWIC games

--- backend/pgnService/explorer/backfillMasterGames.ts
+++ backend/pgnService/explorer/backfillMasterGames.ts
@@ -5,8 +5,8 @@ import { Readable } from 'stream';
 import { close, initialize } from './cache';
 import { explorerGames, processRecord, processed, skipped } from './processGame';
 
-const MIN_FILE = 0;
-const MAX_FILE = 13;
+const MIN_FILE = 1538;
+const MAX_FILE = 1550;
 
 async function main() {
     await initialize();
@@ -14,7 +14,7 @@ async function main() {
     for (let i = MIN_FILE; i < MAX_FILE; i++) {
         console.log(`${new Date().toISOString()} INFO ${i}: starting game file`);
 
-        const fileStream = fs.createReadStream(`/home/ec2-user/games-${i}.json`);
+        const fileStream = fs.createReadStream(`/home/ec2-user/twic_games_${i}.json`);
         const reader = readline.createInterface({ input: fileStream });
         const inputStream = Readable.from(processRecord(i, reader));
         const writeStream = fs.createWriteStream(`output-${i}.json`);
