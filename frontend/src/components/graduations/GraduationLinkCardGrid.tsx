e14fa39922df8ba86f56c22b326c7a2daa50b277
chao-mu <me@queerlyautumn.com>
2024-08-20 22:28:13 -0400

    feat(frontend): graduation index pages

---
+++ frontend/src/components/graduations/GraduationLinkCardGrid.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { GraduationLinkCard } from '@/components/graduations/GraduationLinkCard';
+import { Graduation } from '@/database/graduation';
+import { Grid } from '@mui/material';
+
+interface GraduationLinkCardGridProps {
+    graduations: Graduation[];
+}
+
+export default function GraduationLinkCardGrid({
+    graduations,
+}: GraduationLinkCardGridProps) {
+    graduations.sort(
+        (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    );
+
+    return (
+        <Grid container spacing={2} alignItems='center' justifyContent='center'>
+            {graduations.map((grad) => (
+                <Grid
+                    key={`${grad.username}//${grad.createdAt}`}
+                    item
+                    xs={8}
+                    md={4}
+                    xl={2}
+                >
+                    <GraduationLinkCard
+                        graduation={grad}
+                        to={`/graduations/reports/${grad.previousCohort}/${grad.username}`}
+                    />
+                </Grid>
+            ))}
+        </Grid>
+    );
+}
