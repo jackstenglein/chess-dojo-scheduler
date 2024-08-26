2124487295b1d57687361d35654bd1494a71d2c2
jalpp <jalp.panchal44@gmail.com>
2024-08-25 22:06:08 -0400

    test(use another tournament tab for test)

--- frontend/cypress/e2e/tournaments/leaderboard.cy.ts
+++ frontend/cypress/e2e/tournaments/leaderboard.cy.ts
@@ -2,12 +2,14 @@ import { tournamentsClock } from './util';
 
 describe('Leaderboard Tab', () => {
     beforeEach(() => {
-        // Mock API responses
         cy.interceptApi(
             'GET',
             '/public/tournaments/leaderboard?site=lichess.org&timePeriod=monthly&tournamentType=ARENA&timeControl=blitz&date=2023-09-13T05:00:00.000Z',
-            {
-                fixture: 'tournaments/leaderboardBlitzArenaMonthly.json',
+            (req) => {
+                req.reply((res) => {
+                    console.log(res.body); 
+                    res.send({ fixture: 'tournaments/leaderboardBlitzArenaMonthly.json' });
+                });
             },
         );
         cy.interceptApi(
@@ -18,24 +20,20 @@ describe('Leaderboard Tab', () => {
             },
         );
 
-        // Log in via API
         cy.loginByCognitoApi(
             'tournaments',
             cy.dojo.env('cognito_username'),
             cy.dojo.env('cognito_password'),
         );
 
-        // Set up clock and visit the tournaments page
         cy.clock(tournamentsClock);
         cy.visit('/tournaments?type=leaderboard');
         cy.tick(1000); // Necessary when using cy.clock
     });
 
     it('contains search options', () => {
-        // Wait for the Leaderboard tab to be visible and click it
         cy.contains('Leaderboard', { timeout: 10000 }).should('be.visible').click();
 
-        // Check for the presence of search options
         cy.getBySel('time-control-selector').should('be.visible');
         cy.getBySel('tournament-type-selector').should('be.visible');
         cy.contains('September 2023').should('be.visible');
@@ -44,13 +42,11 @@ describe('Leaderboard Tab', () => {
     });
 
     it('contains correct columns', () => {
-        // Wait for the Leaderboard tab to be visible and click it
         cy.contains('Leaderboard', { timeout: 10000 }).should('be.visible').click();
 
-        // Define the expected column headers
+        
         const columns = ['Rank', 'Username', 'Rating', 'Score'];
 
-        // Check that the correct columns are displayed in the leaderboard
         cy.getBySel('leaderboard')
             .find('.MuiDataGrid-columnHeader')
             .should('have.length', columns.length);
@@ -58,13 +54,17 @@ describe('Leaderboard Tab', () => {
         cy.getBySel('leaderboard').containsAll(columns);
     });
 
-    // it('displays correct data', () => {
-    //     // Wait for the Leaderboard tab to be visible and click it
-    //     cy.contains('Leaderboard', { timeout: 10000 }).should('be.visible').click();
+    it('displays correct data', () => {
+        cy.contains('Leaderboard', { timeout: 10000 }).should('be.visible').click();
+        
+        cy.contains('Yearly').click();
+        cy.getBySel('time-control-selector').click();
+        cy.contains('Blitz', { timeout: 10000 }).click();
+        cy.getBySel('leaderboard').should('be.visible'); 
 
-    //     cy.getBySel('leaderboard').contains('No Rows');
-       
-    // });
+        cy.getBySel('leaderboard').contains('newPlaye');
+        cy.getBySel('leaderboard').contains('1–10 of 40');
+    });
 });
 
 
