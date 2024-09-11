385a8880341344e9d8edb12eeff328070601a466
Jack Stenglein <jackstenglein@gmail.com>
2024-09-10 19:49:50 -0500

    fix(#766): auto focus directory text fields

--- frontend/src/profile/directories/DeleteDialog.tsx
+++ frontend/src/profile/directories/DeleteDialog.tsx
@@ -125,6 +125,7 @@ export const DeleteDialog = ({
                                     }
                                 }}
                                 fullWidth
+                                autoFocus
                             />
                         </>
                     )}
