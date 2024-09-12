d3866306a4f88a1afaae873b78736e8d14e1471c
dmhokie <dmhokie@gmail.com>
2024-09-12 08:46:38 -0400

    Dojo at the olympiad blog
    Blog post about the Olympiad

---
+++ frontend/src/app/(blog)/blog/player-spotlight/dojo-at-olympiad/page.tsx
@@ -0,0 +1,68 @@
+import { Container, Divider, Link, Typography } from '@mui/material';
+import { Metadata } from 'next';
+import { Footer } from '../../common/Footer';
+import { Header } from '../../common/Header';
+
+export const metadata: Metadata = {
+    title: 'Dojo at the Olympiad!',
+    description: `Jan, known in the Dojo as LifeCanBeSoNice got the party started with this amazing AI driven hype video. And Sensei David did a vid about German #1 Vincent Keymer.`,
+};
+
+export default function DojoTalksTop2025() {
+    return (
+        <Container maxWidth='sm' sx={{ py: 5 }}>
+            <Header title='Dojo at the Olympiad!' subtitle='September 12, 2024' />
+
+            <Typography mb={3}>
+                Jan, known in the Dojo as{' '}
+                <Link href='https://www.chessdojo.club/profile/90957cf2-7e8c-43a7-a4f3-f063f24e3781?utm_source=playerspotlight&utm_medium=blog&utm_campaign=lifecanbesonice'>
+                    LifeCanBeSoNice
+                </Link>
+                , got the party started with this amazing AI driven hype video.
+            </Typography>
+
+            <iframe
+                width='100%'
+                style={{ aspectRatio: '16 / 9' }}
+                src='https://www.youtube.com/embed/2jwOHs9DlGQ?si=WWioRGU_ImGiIT7E'
+                title='YouTube video player'
+                frameBorder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen'
+                referrerPolicy='strict-origin-when-cross-origin'
+                allowFullScreen
+            />
+
+            <br />
+            <br />
+            <br />
+
+            <Typography>
+                And Sensei David did a vid about German #1 Vincent Keymer.
+            </Typography>
+            <br />
+            <iframe
+                width='100%'
+                style={{ aspectRatio: '16 / 9' }}
+                src='https://www.youtube.com/embed/9UW7F0czGDk?si=f_AyX9FGfA8QWOoa'
+                title='YouTube video player'
+                frameBorder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen'
+                referrerPolicy='strict-origin-when-cross-origin'
+                allowFullScreen
+            />
+            <br />
+            <br />
+            <Typography>
+                Dojo hopes to do several rounds of{' '}
+                <Link href='https://www.twitch.tv/chessdojo'>Twitch coverage </Link>
+                of the event after sensei Kostya finishes his luxurious Italian norm
+                event, which we are also covering. After Kostya has eaten all the pasta he
+                can eat he’s heading over to Budapest with a fancy press pass. We will
+                have our own reporter on the ground!
+            </Typography>
+            <Divider sx={{ my: 6 }} />
+
+            <Footer utmSource='playerspotlight' utmCampaign='lifecanbesonice' />
+        </Container>
+    );
+}
