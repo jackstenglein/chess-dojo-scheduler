7aa61ed39d17678d91b425e267fd21e5a8e73837
Jack Stenglein <jackstenglein@gmail.com>
2024-09-16 13:49:35 -0500

    refactor(#573): revert changes unrelated to SF 11

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -19,37 +19,6 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-          {
-            
-                source: "/",
-                headers: [
-                  {
-                    key: "Cross-Origin-Embedder-Policy",
-                    value: "require-corp",
-                  },
-                  {
-                    key: "Cross-Origin-Opener-Policy",
-                    value: "same-origin",
-                  },
-                ],
-              },
-              {
-                source: "/engine/:blob*",
-                headers: [
-                  {
-                    key: "Cross-Origin-Embedder-Policy",
-                    value: "require-corp",
-                  },
-                  {
-                    key: "Cross-Origin-Opener-Policy",
-                    value: "same-origin",
-                  },
-                ],
-              },
-        ];
-      },
 };
 
 export default nextConfig;
