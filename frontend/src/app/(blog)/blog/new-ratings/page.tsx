8bd5d3562658d9dc5ccc8031b42d27b6d836aad1
Jack Stenglein <jackstenglein@gmail.com>
2024-09-17 21:31:52 -0500

    fix(blog): lint error

--- frontend/src/app/(blog)/blog/new-ratings/page.tsx
+++ frontend/src/app/(blog)/blog/new-ratings/page.tsx
@@ -23,10 +23,10 @@ export default function NewRatings() {
                     height='315'
                     src='https://www.youtube.com/embed/WUlLQ7MiFrw'
                     title='YouTube video player'
-                    frameborder='0'
+                    frameBorder='0'
                     allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                    referrerpolicy='strict-origin-when-cross-origin'
-                    allowfullscreen
+                    referrerPolicy='strict-origin-when-cross-origin'
+                    allowFullScreen
                 ></iframe>
 
                 <Typography mt={3}>
