0f6342fab34ac2a97409187982954b31a6152973
Jack Stenglein <jackstenglein@gmail.com>
2024-09-18 19:15:57 -0500

    fix(#573): cors error on games page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -19,36 +19,23 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
+    headers() {
         return [
-          {
-            source: "/",
-            headers: [
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp",
-              },
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin",
-              },
-            ],
-          },
-          // {
-          //   source: "/games/:path*", // enable this comment to see the cors issue
-          //   headers: [
-          //     {
-          //       key: "Cross-Origin-Embedder-Policy",
-          //       value: "require-corp",
-          //     },
-          //     {
-          //       key: "Cross-Origin-Opener-Policy",
-          //       value: "same-origin",
-          //     },
-          //   ],
-          // },
+            {
+                source: '/games/:path*',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                ],
+            },
         ];
-      },
+    },
 };
 
 export default nextConfig;
