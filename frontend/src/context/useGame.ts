6876df054b663a852d2cb35e555c4829841efdb5
Autumn <bestieboots@queerlyautumn.com>
2024-10-24 14:07:34 -0400

    refactor: move Game context out of Page component

---
+++ frontend/src/context/useGame.ts
@@ -0,0 +1,14 @@
+import { Game } from '@/database/game';
+import { createContext, useContext } from 'react';
+
+export interface GameContextType {
+    game?: Game;
+    onUpdateGame?: (g: Game) => void;
+    isOwner?: boolean;
+}
+
+export const GameContext = createContext<GameContextType>({});
+
+export default function useGame() {
+    return useContext(GameContext);
+}
