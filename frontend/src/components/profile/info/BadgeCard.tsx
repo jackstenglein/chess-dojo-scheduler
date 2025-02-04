7c0e0ae1f9158246e0dbad069f400535a42a2c7d
jalpp <jalp.panchal44@gmail.com>
2025-02-04 02:55:23 +0000

    fix(badges): fix badge pop up trigger

--- frontend/src/components/profile/info/BadgeCard.tsx
+++ frontend/src/components/profile/info/BadgeCard.tsx
@@ -1,112 +1,170 @@
-import { useState } from 'react';
-import { Dialog, DialogContent, DialogTitle, Tooltip, Card, CardContent, CardHeader, Stack, IconButton } from '@mui/material';
-import { Close as CloseIcon } from '@mui/icons-material';
-import Image from 'next/image';
 import { Link } from '@/components/navigation/Link';
 import { User, compareCohorts } from '@/database/user';
 import CohortIcon from '@/scoreboard/CohortIcon';
+import { Close as CloseIcon } from '@mui/icons-material';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    IconButton,
+    Stack,
+    Tooltip,
+    Typography,
+} from '@mui/material';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import postmortem2023 from './2023-postmortem.png';
 import postmortem2024 from './2024-postmortem.png';
 import { getEligibleBadges } from './BadgeHandler';
 
 export const BadgeCard = ({ user }: { user: User }) => {
-  const [openDialog, setOpenDialog] = useState(false);
-  const [selectedBadge, setSelectedBadge] = useState<string[] | null>(null);
-  const badgeImgs: string[][] = getEligibleBadges(user);
-  console.log(badgeImgs);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selectedBadge, setSelectedBadge] = useState<string[] | null>(null);
 
-  const handleBadgeClick = (badge: string[]) => {
-    setSelectedBadge(badge);
-    setOpenDialog(true);
-  };
+    const badgeImgs: string[][] = getEligibleBadges(user);
+    const [previousBadgeImgs, setPreviousBadgeImgs] = useState<string[][]>(badgeImgs);
 
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-    setSelectedBadge(null);
-  };
+    const handleBadgeClick = (badge: string[]) => {
+        setSelectedBadge(badge);
+        setOpenDialog(true);
+    };
 
-  const badges =
-    user.graduationCohorts?.sort(compareCohorts)
-      .filter((c, i) => user.graduationCohorts?.indexOf(c) === i)
-      .map((c) => (
-        <CohortIcon key={c} cohort={c} />
-      )) ?? [];
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+        setSelectedBadge(null);
+    };
 
-  if (!user.createdAt || user.createdAt < '2024-12') {
-    badges.push(
-      <Link key='postmortem-2024' href={`/profile/${user.username}/postmortem/2024`}>
-        <Tooltip title='View my 2024 postmortem!'>
-          <Image src={postmortem2024} alt='2024 postmortem' width={40} height={40} />
-        </Tooltip>
-      </Link>
-    );
-  }
+    useEffect(() => {
+        const newBadge = badgeImgs.find(
+            (badge, index) =>
+                !previousBadgeImgs[index] || badge[0] !== previousBadgeImgs[index][0],
+        );
+        if (newBadge) {
+            setSelectedBadge(newBadge);
+            setOpenDialog(true);
+        }
 
-  if (!user.createdAt || user.createdAt < '2023-12') {
-    badges.push(
-      <Link key='postmortem-2023' href={`/profile/${user.username}/postmortem/2023`}>
-        <Tooltip title='View my 2023 postmortem!'>
-          <Image src={postmortem2023} alt='2023 postmortem' width={70} height={70} />
-        </Tooltip>
-      </Link>
-    );
-  }
+        setPreviousBadgeImgs([...badgeImgs]);
+    }, [badgeImgs]);
 
-  if (badgeImgs.length !== 0) {
-    for (let i = 0; i < badgeImgs.length; i++) {
-      badges.push(
-        <Tooltip title={badgeImgs[i][1]}>
-          <img
-            src={badgeImgs[i][0]}
-            style={{ height: '50px', width: '50px', cursor: 'pointer' }}
-            alt={badgeImgs[i][1]}
-            onClick={() => handleBadgeClick(badgeImgs[i])}
-          />
-        </Tooltip>
-      );
+    const badges =
+        user.graduationCohorts
+            ?.sort(compareCohorts)
+            .filter((c, i) => user.graduationCohorts?.indexOf(c) === i)
+            .map((c) => <CohortIcon key={c} cohort={c} />) ?? [];
+
+    if (!user.createdAt || user.createdAt < '2024-12') {
+        badges.push(
+            <Link
+                key='postmortem-2024'
+                href={`/profile/${user.username}/postmortem/2024`}
+            >
+                <Tooltip title='View my 2024 postmortem!'>
+                    <Image
+                        src={postmortem2024}
+                        alt='2024 postmortem'
+                        width={40}
+                        height={40}
+                    />
+                </Tooltip>
+            </Link>,
+        );
     }
-  }
 
-  if (badges.length === 0) {
-    return null;
-  }
+    if (!user.createdAt || user.createdAt < '2023-12') {
+        badges.push(
+            <Link
+                key='postmortem-2023'
+                href={`/profile/${user.username}/postmortem/2023`}
+            >
+                <Tooltip title='View my 2023 postmortem!'>
+                    <Image
+                        src={postmortem2023}
+                        alt='2023 postmortem'
+                        width={70}
+                        height={70}
+                    />
+                </Tooltip>
+            </Link>,
+        );
+    }
 
-  return (
-    <>
-      <Card>
-        <CardHeader title='Badges' />
-        <CardContent sx={{ pt: 0 }}>
-          <Stack direction='row' columnGap={0.75} flexWrap='wrap' rowGap={1}>
-            {badges}
-          </Stack>
-        </CardContent>
-      </Card>
+    if (badgeImgs.length !== 0) {
+        for (let i = 0; i < badgeImgs.length; i++) {
+            badges.push(
+                <Tooltip title={badgeImgs[i][1]} key={badgeImgs[i][0]}>
+                    <img
+                        src={badgeImgs[i][0]}
+                        style={{ height: '50px', width: '50px', cursor: 'pointer' }}
+                        alt={badgeImgs[i][1]}
+                        onClick={() => handleBadgeClick(badgeImgs[i])}
+                    />
+                </Tooltip>,
+            );
+        }
+    }
 
-      <Dialog open={openDialog} onClose={handleCloseDialog}>
-        {selectedBadge && (
-          <>
-            <DialogTitle>
-              {selectedBadge[1]}
-              <IconButton
-                aria-label="close"
-                onClick={handleCloseDialog}
-                sx={{
-                  position: 'absolute',
-                  right: 8,
-                  top: 8,
-                  color: (theme) => theme.palette.grey[500],
-                }}
-              >
-                <CloseIcon />
-              </IconButton>
-            </DialogTitle>
-            <DialogContent>
-              <img src={selectedBadge[0]} alt={selectedBadge[1]} style={{ maxWidth: '100%' }} />
-            </DialogContent>
-          </>
-        )}
-      </Dialog>
-    </>
-  );
+    if (badges.length === 0) {
+        return null;
+    }
+
+    return (
+        <>
+            <Card>
+                <CardHeader title='Badges' />
+                <CardContent sx={{ pt: 0 }}>
+                    <Stack direction='row' columnGap={0.75} flexWrap='wrap' rowGap={1}>
+                        {badges}
+                    </Stack>
+                </CardContent>
+            </Card>
+
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                {selectedBadge && (
+                    <>
+                        <DialogTitle
+                            sx={{
+                                backgroundColor: '#222222',
+                                textAlign: 'center',
+                                color: '#EEB312',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {selectedBadge[1]}
+                            <IconButton
+                                aria-label='close'
+                                onClick={handleCloseDialog}
+                                sx={{
+                                    position: 'absolute',
+                                    right: 8,
+                                    top: 8,
+                                    color: (theme) => theme.palette.grey[500],
+                                }}
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                        </DialogTitle>
+                        <DialogContent
+                            sx={{
+                                backgroundColor: '#222222',
+                                textAlign: 'center',
+                            }}
+                        >
+                            <img
+                                src={selectedBadge[0]}
+                                alt={selectedBadge[1]}
+                                style={{ maxWidth: '80%', borderRadius: '10px' }}
+                            />
+                            <Typography variant='body1' sx={{ mt: 2, color: '#EEB312' }}>
+                                {selectedBadge[2]}
+                            </Typography>
+                        </DialogContent>
+                    </>
+                )}
+            </Dialog>
+        </>
+    );
 };
-
--- frontend/src/components/profile/info/BadgeHandler.ts
+++ frontend/src/components/profile/info/BadgeHandler.ts
@@ -1,4 +1,3 @@
-
 import { User } from '@/database/user';
 
 /**
@@ -18,11 +17,70 @@ export enum BADGE {
  */
 const BADGE_LIMITS: Record<BADGE, number[]> = {
     [BADGE.POLGAR_MATE_ONE]: [50, 306],
-    [BADGE.POLGAR_MATE_TWO]: [500, 750, 1471, 2000, 2500, 3000],
+    [BADGE.POLGAR_MATE_TWO]: [500, 750, 1471, 2000, 2500, 3000, 3718],
     [BADGE.POLGAR_MATE_THREE]: [4462],
     [BADGE.CLASSICAL_GAMES]: [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500],
     [BADGE.ANNONTATE_GAMES]: [1, 5, 10, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500],
     [BADGE.DAILY_STREAK]: [3, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65],
+};
+
+/**
+ * classical games level to message hashmap
+ */
+
+const classicalGameMilestones: Record<number, string> = {
+    1: 'Congrats on playing your first classical game. Keep up the good work!',
+    5: "Congrats on playing five classical games. That's a full tournament!",
+    10: "Congrats on playing 10 classical games! You're on your way!",
+    25: "Wow! 25 classical games—you're really dedicated!",
+    50: "Congrats on reaching 50 classical games! That's impressive!",
+    75: "You're unstoppable! 75 classical games played!",
+    100: "Amazing! You've played 100 classical games—century club!",
+    150: '150 classical games! Your dedication is truly inspiring!',
+    200: "200 classical games—you're a seasoned player now!",
+    250: 'Incredible! 250 classical games! A true chess warrior!',
+    300: '300 classical games! Your endurance is unmatched!',
+    400: '400 classical games—masterful commitment!',
+    500: "500 classical games! You're a true legend!",
+};
+
+/**
+ *
+ * annontation to message hashmap
+ */
+
+const annotationMilestones: Record<number, string> = {
+    1: 'Congrats on annotating your first classical game. Keep up the good work!',
+    5: "Congrats on annotating five classical games. That's a strong start!",
+    10: "Congrats on annotating 10 classical games! You're building valuable insights!",
+    25: 'Wow! 25 classical game annotations—your analysis is impressive!',
+    50: 'Congrats on reaching 50 annotated classical games! A true analyst!',
+    75: "You're a dedicated student of the game! Whoa, check out Fischer over here!",
+    100: 'Amazing! 100 classical game annotations—insightful mastery! Whoa, check out Botvinnik over here!',
+    150: '150 annotations! Your analytical skills are truly inspiring!',
+    200: "200 classical game annotations—you're shaping your chess mind!",
+    250: 'Incredible! 250 annotations! A true chess scholar!',
+    300: '300 annotations! Whoa, check out Tarrasch over here!!',
+    400: '400 classical game annotations—remarkable dedication!',
+    500: '500 classical game annotations! Whoa, check out Tartakower over here!',
+};
+
+/**
+ * Polgar to message hashmap
+ */
+
+const polgarMateMilestones: Record<number, string> = {
+    50: "Congrats on solving your first 50 Polgar mates! You're sharpening your tactical vision.",
+    306: "Amazing! You've completed all mate-in-ones. Great job recognizing immediate threats!",
+    500: '500 Polgar mates solved! Your tactical acumen is growing rapidly.',
+    750: "750 Polgar mates! That's impressive dedication to tactical mastery.",
+    1000: '1,000 Polgar mates solved! Your chess instincts are getting sharper!',
+    1471: "Incredible! You've solved all simple mate-in-twos. Tactical brilliance!",
+    2000: '2,000 Polgar mates solved—truly remarkable tactical prowess!',
+    2500: "2,500 Polgar mates! You're a tactical mastermind!",
+    3000: "3,000 Polgar mates solved—you're relentless in your pursuit of tactical excellence!",
+    3718: "Outstanding! You've conquered all mate-in-twos. Tactical genius unlocked!",
+    4462: "Amazing achievement! You've mastered all mate-in-threes. Tactical artistry!",
 };
 
 /**
@@ -124,6 +182,38 @@ function isEligibleForLimit(user: User, reqid: string, levels: number[]): number
 }
 
 /**
+ * gets the badge message
+ * @param level the level for badge
+ * @param badge the badge type
+ * @returns the message
+ */
+function getBadgeMessage(level: number, badge: BADGE): string {
+    let msg: string;
+    switch (badge) {
+        case BADGE.POLGAR_MATE_ONE:
+            msg = polgarMateMilestones[level];
+            break;
+        case BADGE.POLGAR_MATE_TWO:
+            msg = polgarMateMilestones[level];
+            break;
+        case BADGE.POLGAR_MATE_THREE:
+            msg = polgarMateMilestones[level];
+            break;
+        case BADGE.ANNONTATE_GAMES:
+            msg = annotationMilestones[level];
+            break;
+        case BADGE.CLASSICAL_GAMES:
+            msg = classicalGameMilestones[level];
+            break;
+        case BADGE.DAILY_STREAK:
+            msg = 'Wowza! Your daily streak hit ' + level + '!';
+            break;
+    }
+
+    return msg;
+}
+
+/**
  * gets the info and badge image for eligible badge
  * @param user current user
  * @param badge the badge type
@@ -138,6 +228,7 @@ function isEligibleBadgeImage(user: User, badge: BADGE): string[] | undefined {
 
     info.push(getBadgeImage(level, badge));
     info.push(BADGE_TITLE[badge] + ' ' + level);
+    info.push(getBadgeMessage(level, badge));
 
     return info;
 }
@@ -159,4 +250,3 @@ export function getEligibleBadges(user: User): string[][] {
 
     return overallInfo;
 }
-
