0ad2a03b13ffecdf044e8be7651c0c10aa4172dc
Jack Stenglein <jackstenglein@gmail.com>
2025-03-24 15:59:46 -0500

    fix: update vulnerable package versions (#1334)
    * test updating EnvSchema import
    
    * test directly setting cypress env
    
    * fix: update vulnerable package versions

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,7 +1,6 @@
 // import task from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
 import dotenv from 'dotenv';
-import { EnvSchema } from './cypress/env';
 
 dotenv.config({
     path: ['.env.test.local', '.env.local', '.env.test', '.env'],
@@ -17,26 +16,23 @@ export default defineConfig({
         experimentalRunAllSpecs: true,
     },
 
-    env: EnvSchema.strict().parse({
+    env: {
         apiBaseUrl: 'https://c2qamdaw08.execute-api.us-east-1.amazonaws.com',
         numCohorts: 23,
 
         cognito_username: env.AWS_COGNITO_USERNAME,
         cognito_password: env.AWS_COGNITO_PASSWORD,
         cognito_region: env.NEXT_PUBLIC_AUTH_REGION,
-        cognito_user_pool_id:
-            env.AWS_COGNITO_USER_POOL_ID ?? env.NEXT_PUBLIC_AUTH_USER_POOL_ID,
+        cognito_user_pool_id: env.AWS_COGNITO_USER_POOL_ID ?? env.NEXT_PUBLIC_AUTH_USER_POOL_ID,
         cognito_user_pool_web_client_id:
-            env.AWS_COGNITO_USER_POOL_WEB_CLIENT_ID ??
-            env.NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID,
-        cognito_domain:
-            env.AWS_COGNITO_DOMAIN ?? process.env.NEXT_PUBLIC_AUTH_OAUTH_DOMAIN,
+            env.AWS_COGNITO_USER_POOL_WEB_CLIENT_ID ?? env.NEXT_PUBLIC_AUTH_USER_POOL_WEB_CLIENT_ID,
+        cognito_domain: env.AWS_COGNITO_DOMAIN ?? process.env.NEXT_PUBLIC_AUTH_OAUTH_DOMAIN,
 
         codeCoverage: {
             exclude: 'cypress/**/*.*',
             url: 'http://localhost:3000/api/__coverage__',
         },
-    }),
+    },
 
     retries: {
         runMode: 2,
