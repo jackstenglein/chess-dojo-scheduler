ce68a83548f3b58e8973ed04d74d02e7bc9cf80d
Jack Stenglein <jackstenglein@gmail.com>
2025-04-03 20:12:37 -0500

    fix(#1195): delete unused file

--- frontend/src/board/pgn/boardTools/underboard/comments/SuggestedVariation.tsx
+++
@@ -1,7 +0,0 @@
-import Lines from '@/board/pgn/pgnText/Lines';
-import { Chess } from '@jackstenglein/chess';
-
-export function SuggestedVariation({ pgn }: { pgn: string }) {
-    const variationChess = new Chess({ pgn });
-    return <Lines lines={[variationChess.history()]} handleScroll={() => null} />;
-}
