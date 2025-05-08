b730bcf36552e4b7c69aa9901a1d27a24cb57c98
jalpp <jalp.panchal44@gmail.com>
2025-05-07 20:45:02 -0400

    feat(performancerating): add performance rating

---
+++ backend/directoryService/rating.ts
@@ -0,0 +1,347 @@
+'use strict';
+
+import {
+    compareRoles,
+    Directory,
+    DirectoryAccessRole,
+    DirectoryItemTypes,
+    DirectoryVisibility,
+} from '@jackstenglein/chess-dojo-common/src/database/directory';
+
+import { RatingSystem, getCohortForGivenRating, getNormalizedRating } from './ratingtypes';
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+import { getAccessRole } from './access';
+import {
+    ApiError,
+    errToApiGatewayProxyResultV2,
+    parsePathParameters,
+    requireUserInfo,
+    success,
+} from './api';
+import { fetchDirectory, getDirectorySchema } from './get';
+
+/**
+ * Handles requests to the get directory API. Returns an error if the directory does
+ * not exist or is private and the caller does not have viewer access.
+ * @param event The API gateway event that triggered the request.
+ * @returns The requested directory.
+ */
+export const handlerV2: APIGatewayProxyHandlerV2 = async (event) => {
+    try {
+        console.log('Event: %j', event);
+
+        const userInfo = requireUserInfo(event);
+        const request = parsePathParameters(event, getDirectorySchema);
+        let directory = await fetchDirectory(request.owner, request.id);
+
+        if (!directory) {
+            throw new ApiError({
+                statusCode: 404,
+                publicMessage: 'Directory not found',
+            });
+        }
+
+        const accessRole = await getAccessRole({
+            owner: directory.owner,
+            id: directory.id,
+            username: userInfo.username,
+            directory,
+        });
+        const isViewer = compareRoles(DirectoryAccessRole.Viewer, accessRole);
+
+        if (directory.visibility === DirectoryVisibility.PRIVATE && !isViewer) {
+            throw new ApiError({
+                statusCode: 403,
+                publicMessage:
+                    'This directory is private. Performance rating can not be generated.',
+            });
+        }
+
+        return success({ directory, accessRole });
+    } catch (err) {
+        return errToApiGatewayProxyResultV2(err);
+    }
+};
+
+interface PerformanceRatingMetric {
+    combinedRating: number,
+    combinedScorePercent: number,
+    normalizedCombinedRating: number,
+    whiteRating: number,
+    whiteScorePercent: number,
+    normalizedWhiteRating: number,
+    blackRating: number,
+    blackScorePercent: number,
+    normalizedBlackRating: number,
+    winRatio: number,
+    drawRatio: number, 
+    lossRatio: number
+    equalCohortRating: number,
+    previousCohortRating: number,
+    prePreviousCohortRating: number,
+    nextCohortRating: number,
+    nextNextCohortRating: number
+}
+
+const fideDpTable: Record<number, number> = {
+    1.00: 800,
+    0.99: 677,
+    0.98: 589,
+    0.97: 538,
+    0.96: 501,
+    0.95: 470,
+    0.94: 444,
+    0.93: 422,
+    0.92: 401,
+    0.91: 383,
+    0.90: 366,
+    0.89: 351,
+    0.88: 336,
+    0.87: 322,
+    0.86: 309,
+    0.85: 296,
+    0.84: 284,
+    0.83: 273,
+    0.82: 262,
+    0.81: 251,
+    0.80: 240,
+    0.79: 230,
+    0.78: 220,
+    0.77: 211,
+    0.76: 202,
+    0.75: 193,
+    0.74: 184,
+    0.73: 175,
+    0.72: 166,
+    0.71: 158,
+    0.70: 149,
+    0.69: 141,
+    0.68: 133,
+    0.67: 125,
+    0.66: 117,
+    0.65: 110,
+    0.64: 102,
+    0.63: 95,
+    0.62: 87,
+    0.61: 80,
+    0.60: 72,
+    0.59: 65,
+    0.58: 57,
+    0.57: 50,
+    0.56: 43,
+    0.55: 36,
+    0.54: 29,
+    0.53: 21,
+    0.52: 14,
+    0.51: 7,
+    0.50: 0,
+    0.49: -7,
+    0.48: -14,
+    0.47: -21,
+    0.46: -29,
+    0.45: -36,
+    0.44: -43,
+    0.43: -50,
+    0.42: -57,
+    0.41: -65,
+    0.40: -72,
+    0.39: -80,
+    0.38: -87,
+    0.37: -95,
+    0.36: -102,
+    0.35: -110,
+    0.34: -117,
+    0.33: -125,
+    0.32: -133,
+    0.31: -141,
+    0.30: -149,
+    0.29: -158,
+    0.28: -166,
+    0.27: -175,
+    0.26: -184,
+    0.25: -193,
+    0.24: -202,
+    0.23: -211,
+    0.22: -220,
+    0.21: -230,
+    0.20: -240,
+    0.19: -251,
+    0.18: -262,
+    0.17: -273,
+    0.16: -284,
+    0.15: -296,
+    0.14: -309,
+    0.13: -322,
+    0.12: -336,
+    0.11: -351,
+    0.10: -366,
+    0.09: -383,
+    0.08: -401,
+    0.07: -422,
+    0.06: -444,
+    0.05: -470,
+    0.04: -501,
+    0.03: -538,
+    0.02: -589,
+    0.01: -677,
+    0.00: -800
+  };
+  
+
+
+export function getPerformanceRating(
+    playername: string,
+    userDirectory: Directory,
+    ratingSystem: RatingSystem,
+    playerCohort: string,
+): PerformanceRatingMetric {
+    const defaultRating = 1500;
+    let wins = 0, whiteWins = 0, blackWins = 0;
+    let draws = 0, whiteDraws = 0, blackDraws = 0;
+    let losses = 0, whiteLoss = 0, blackLoss = 0;
+    let total = 0;
+
+    const cohortWDLCounts: Record<string, number[]> = {
+        playerCohort: [0, 0, 0],
+        previousCohort: [0, 0, 0],
+        prepreviousCohort: [0, 0, 0],
+        nextCohort: [0, 0, 0],
+        nextNextCohort: [0, 0, 0],
+    };
+
+    const cohortRanges: Record<string, number[]> = {
+        playerCohort: [],
+        previousCohort: [],
+        prepreviousCohort: [],
+        nextCohort: [],
+        nextNextCohort: [],
+    };
+
+    const oppAvgRating: number[] = [];
+    const oppBlackAvgRating: number[] = [];
+    const oppWhiteAvgRating: number[] = [];
+
+    const currentCohortStart = parseInt(playerCohort.split('-')[0]);
+
+    userDirectory.itemIds.forEach((currentId) => {
+        const currentItem = userDirectory.items[currentId];
+        if (currentItem.type !== DirectoryItemTypes.OWNED_GAME || !currentItem.metadata.result) {
+            return;
+        }
+
+        total++;
+        const { white, black, result, whiteElo, blackElo } = currentItem.metadata;
+
+        const updateStats = (isWin: boolean, isDraw: boolean, isWhite: boolean, oppElo: string | undefined) => {
+            const rating = oppElo ? parseInt(oppElo) : defaultRating;
+            const oppCohort = getCohortForGivenRating(rating, ratingSystem);
+            const oppCohortStart = oppCohort ? parseInt(oppCohort.split('-')[0]) : 0;
+
+            const cohortKey = oppCohortStart === currentCohortStart + 200
+                ? 'nextNextCohort'
+                : oppCohortStart === currentCohortStart + 100
+                ? 'nextCohort'
+                : oppCohortStart === currentCohortStart
+                ? 'playerCohort'
+                : oppCohortStart === currentCohortStart - 100
+                ? 'previousCohort'
+                : oppCohortStart === currentCohortStart - 200
+                ? 'prepreviousCohort'
+                : null;
+
+            if (cohortKey) {
+                cohortRanges[cohortKey].push(rating);
+                cohortWDLCounts[cohortKey][isWin ? 0 : isDraw ? 1 : 2]++;
+            }
+
+            oppAvgRating.push(rating);
+            (isWhite ? oppBlackAvgRating : oppWhiteAvgRating).push(rating);
+
+            if (isWin) {
+                wins++;
+                isWhite ? whiteWins++ : blackWins++;
+            } else if (isDraw) {
+                draws++;
+                isWhite ? whiteDraws++ : blackDraws++;
+            } else {
+                losses++;
+                isWhite ? whiteLoss++ : blackLoss++;
+            }
+        };
+
+        if (white.includes(playername)) {
+            if (result === "1-0") updateStats(true, false, true, blackElo);
+            else if (result === "0-1") updateStats(false, false, true, blackElo);
+            else if (result === "1/2-1/2") updateStats(false, true, true, blackElo);
+        } else if (black.includes(playername)) {
+            if (result === "0-1") updateStats(true, false, false, whiteElo);
+            else if (result === "1-0") updateStats(false, false, false, whiteElo);
+            else if (result === "1/2-1/2") updateStats(false, true, false, whiteElo);
+        }
+    });
+
+    if (total === 0 || oppAvgRating.length === 0) {
+        return {
+            combinedRating: 0,
+            combinedScorePercent: 0,
+            normalizedCombinedRating: 0,
+            whiteRating: 0,
+            whiteScorePercent: 0,
+            normalizedWhiteRating: 0,
+            blackRating: 0,
+            blackScorePercent: 0,
+            normalizedBlackRating: 0,
+            winRatio: 0,
+            drawRatio: 0,
+            lossRatio: 0,
+            equalCohortRating: 0,
+            previousCohortRating: 0,
+            prePreviousCohortRating: 0,
+            nextCohortRating: 0,
+            nextNextCohortRating: 0,
+        };
+    }
+
+    const calculateAverage = (ratings: number[]) =>
+        Math.round(ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length);
+
+    const totalAvg = calculateAverage(oppAvgRating);
+    const totalWhiteAvg = calculateAverage(oppBlackAvgRating);
+    const totalBlackAvg = calculateAverage(oppWhiteAvgRating);
+
+    const calculateCohortAverage = (key: string) =>
+        calculateAverage(cohortRanges[key]);
+
+    const totalEqualCohortAvg = calculateCohortAverage('playerCohort');
+    const totalPreviousCohortAvg = calculateCohortAverage('previousCohort');
+    const totalPrePreviousCohortAvg = calculateCohortAverage('prepreviousCohort');
+    const totalNextCohortAvg = calculateCohortAverage('nextCohort');
+    const totalNextNextCohortAvg = calculateCohortAverage('nextNextCohort');
+
+    const totalScorePercent = parseFloat(((wins + draws + losses) / total).toFixed(2));
+    const totalWhiteScorePercent = parseFloat(((whiteWins + whiteDraws + whiteLoss) / total).toFixed(2));
+    const totalBlackScorePercent = parseFloat(((blackWins + blackDraws + blackLoss) / total).toFixed(2));
+
+    const calculateRating = (avg: number, scorePercent: number) =>
+        avg + fideDpTable[scorePercent];
+
+    return {
+        combinedRating: calculateRating(totalAvg, totalScorePercent),
+        combinedScorePercent: totalScorePercent,
+        normalizedCombinedRating: getNormalizedRating(calculateRating(totalAvg, totalScorePercent), ratingSystem),
+        whiteRating: calculateRating(totalWhiteAvg, totalWhiteScorePercent),
+        whiteScorePercent: totalWhiteScorePercent,
+        normalizedWhiteRating: getNormalizedRating(calculateRating(totalWhiteAvg, totalWhiteScorePercent), ratingSystem),
+        blackRating: calculateRating(totalBlackAvg, totalBlackScorePercent),
+        blackScorePercent: totalBlackScorePercent,
+        normalizedBlackRating: getNormalizedRating(calculateRating(totalBlackAvg, totalBlackScorePercent), ratingSystem),
+        winRatio: parseFloat((wins / total).toFixed(2)),
+        drawRatio: parseFloat((draws / total).toFixed(2)),
+        lossRatio: parseFloat((losses / total).toFixed(2)),
+        equalCohortRating: calculateRating(totalEqualCohortAvg, totalScorePercent),
+        previousCohortRating: calculateRating(totalPreviousCohortAvg, totalScorePercent),
+        prePreviousCohortRating: calculateRating(totalPrePreviousCohortAvg, totalScorePercent),
+        nextCohortRating: calculateRating(totalNextCohortAvg, totalScorePercent),
+        nextNextCohortRating: calculateRating(totalNextNextCohortAvg, totalScorePercent),
+    };
+}
---
+++ backend/directoryService/ratingtypes.ts
@@ -0,0 +1,487 @@
+
+export const dojoCohorts = [
+    '0-300',
+    '300-400',
+    '400-500',
+    '500-600',
+    '600-700',
+    '700-800',
+    '800-900',
+    '900-1000',
+    '1000-1100',
+    '1100-1200',
+    '1200-1300',
+    '1300-1400',
+    '1400-1500',
+    '1500-1600',
+    '1600-1700',
+    '1700-1800',
+    '1800-1900',
+    '1900-2000',
+    '2000-2100',
+    '2100-2200',
+    '2200-2300',
+    '2400+',
+];
+
+export enum RatingSystem {
+    Chesscom = 'CHESSCOM',
+    Lichess = 'LICHESS',
+    Fide = 'FIDE',
+    Uscf = 'USCF',
+    Ecf = 'ECF',
+    Cfc = 'CFC',
+    Dwz = 'DWZ',
+    Acf = 'ACF',
+    Knsb = 'KNSB',
+    Custom = 'CUSTOM',
+    Custom2 = 'CUSTOM_2',
+    Custom3 = 'CUSTOM_3',
+}
+
+
+const ratingBoundaries: Record<string, Record<RatingSystem, number>> = {
+    '0-300': {
+        [RatingSystem.Chesscom]: 550,
+        [RatingSystem.Lichess]: 1250,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 350,
+        [RatingSystem.Ecf]: 400,
+        [RatingSystem.Cfc]: 350,
+        [RatingSystem.Dwz]: 450,
+        [RatingSystem.Acf]: 300,
+        [RatingSystem.Knsb]: 400,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '300-400': {
+        [RatingSystem.Chesscom]: 650,
+        [RatingSystem.Lichess]: 1310,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 460,
+        [RatingSystem.Ecf]: 625,
+        [RatingSystem.Cfc]: 460,
+        [RatingSystem.Dwz]: 540,
+        [RatingSystem.Acf]: 395,
+        [RatingSystem.Knsb]: 600,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '400-500': {
+        [RatingSystem.Chesscom]: 750,
+        [RatingSystem.Lichess]: 1370,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 570,
+        [RatingSystem.Ecf]: 850,
+        [RatingSystem.Cfc]: 570,
+        [RatingSystem.Dwz]: 630,
+        [RatingSystem.Acf]: 490,
+        [RatingSystem.Knsb]: 800,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '500-600': {
+        [RatingSystem.Chesscom]: 850,
+        [RatingSystem.Lichess]: 1435,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 680,
+        [RatingSystem.Ecf]: 1000,
+        [RatingSystem.Cfc]: 680,
+        [RatingSystem.Dwz]: 725,
+        [RatingSystem.Acf]: 585,
+        [RatingSystem.Knsb]: 1000,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '600-700': {
+        [RatingSystem.Chesscom]: 950,
+        [RatingSystem.Lichess]: 1500,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 790,
+        [RatingSystem.Ecf]: 1130,
+        [RatingSystem.Cfc]: 780,
+        [RatingSystem.Dwz]: 815,
+        [RatingSystem.Acf]: 680,
+        [RatingSystem.Knsb]: 1140,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '700-800': {
+        [RatingSystem.Chesscom]: 1050,
+        [RatingSystem.Lichess]: 1550,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 900,
+        [RatingSystem.Ecf]: 1210,
+        [RatingSystem.Cfc]: 880,
+        [RatingSystem.Dwz]: 920,
+        [RatingSystem.Acf]: 775,
+        [RatingSystem.Knsb]: 1280,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '800-900': {
+        [RatingSystem.Chesscom]: 1150,
+        [RatingSystem.Lichess]: 1600,
+        [RatingSystem.Fide]: 0,
+        [RatingSystem.Uscf]: 1010,
+        [RatingSystem.Ecf]: 1270,
+        [RatingSystem.Cfc]: 980,
+        [RatingSystem.Dwz]: 1025,
+        [RatingSystem.Acf]: 870,
+        [RatingSystem.Knsb]: 1400,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '900-1000': {
+        [RatingSystem.Fide]: 1450,
+        [RatingSystem.Uscf]: 1120,
+        [RatingSystem.Chesscom]: 1250,
+        [RatingSystem.Lichess]: 1665,
+        [RatingSystem.Ecf]: 1325,
+        [RatingSystem.Cfc]: 1090,
+        [RatingSystem.Dwz]: 1110,
+        [RatingSystem.Acf]: 990,
+        [RatingSystem.Knsb]: 1450,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1000-1100': {
+        [RatingSystem.Fide]: 1500,
+        [RatingSystem.Uscf]: 1230,
+        [RatingSystem.Chesscom]: 1350,
+        [RatingSystem.Lichess]: 1730,
+        [RatingSystem.Ecf]: 1390,
+        [RatingSystem.Cfc]: 1200,
+        [RatingSystem.Dwz]: 1185,
+        [RatingSystem.Acf]: 1100,
+        [RatingSystem.Knsb]: 1500,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1100-1200': {
+        [RatingSystem.Fide]: 1550,
+        [RatingSystem.Uscf]: 1330,
+        [RatingSystem.Chesscom]: 1450,
+        [RatingSystem.Lichess]: 1795,
+        [RatingSystem.Ecf]: 1455,
+        [RatingSystem.Cfc]: 1300,
+        [RatingSystem.Dwz]: 1260,
+        [RatingSystem.Acf]: 1210,
+        [RatingSystem.Knsb]: 1550,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1200-1300': {
+        [RatingSystem.Fide]: 1600,
+        [RatingSystem.Uscf]: 1420,
+        [RatingSystem.Chesscom]: 1550,
+        [RatingSystem.Lichess]: 1850,
+        [RatingSystem.Ecf]: 1535,
+        [RatingSystem.Cfc]: 1390,
+        [RatingSystem.Dwz]: 1335,
+        [RatingSystem.Acf]: 1320,
+        [RatingSystem.Knsb]: 1600,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1300-1400': {
+        [RatingSystem.Fide]: 1650,
+        [RatingSystem.Uscf]: 1510,
+        [RatingSystem.Chesscom]: 1650,
+        [RatingSystem.Lichess]: 1910,
+        [RatingSystem.Ecf]: 1595,
+        [RatingSystem.Cfc]: 1480,
+        [RatingSystem.Dwz]: 1410,
+        [RatingSystem.Acf]: 1415,
+        [RatingSystem.Knsb]: 1650,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1400-1500': {
+        [RatingSystem.Fide]: 1700,
+        [RatingSystem.Uscf]: 1600,
+        [RatingSystem.Chesscom]: 1750,
+        [RatingSystem.Lichess]: 1970,
+        [RatingSystem.Ecf]: 1665,
+        [RatingSystem.Cfc]: 1570,
+        [RatingSystem.Dwz]: 1480,
+        [RatingSystem.Acf]: 1510,
+        [RatingSystem.Knsb]: 1700,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1500-1600': {
+        [RatingSystem.Fide]: 1750,
+        [RatingSystem.Uscf]: 1675,
+        [RatingSystem.Chesscom]: 1850,
+        [RatingSystem.Lichess]: 2030,
+        [RatingSystem.Ecf]: 1735,
+        [RatingSystem.Cfc]: 1645,
+        [RatingSystem.Dwz]: 1560,
+        [RatingSystem.Acf]: 1605,
+        [RatingSystem.Knsb]: 1750,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1600-1700': {
+        [RatingSystem.Fide]: 1800,
+        [RatingSystem.Uscf]: 1750,
+        [RatingSystem.Chesscom]: 1950,
+        [RatingSystem.Lichess]: 2090,
+        [RatingSystem.Ecf]: 1805,
+        [RatingSystem.Cfc]: 1730,
+        [RatingSystem.Dwz]: 1640,
+        [RatingSystem.Acf]: 1700,
+        [RatingSystem.Knsb]: 1800,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1700-1800': {
+        [RatingSystem.Fide]: 1850,
+        [RatingSystem.Uscf]: 1825,
+        [RatingSystem.Chesscom]: 2050,
+        [RatingSystem.Lichess]: 2150,
+        [RatingSystem.Ecf]: 1875,
+        [RatingSystem.Cfc]: 1825,
+        [RatingSystem.Dwz]: 1720,
+        [RatingSystem.Acf]: 1790,
+        [RatingSystem.Knsb]: 1850,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1800-1900': {
+        [RatingSystem.Fide]: 1910,
+        [RatingSystem.Uscf]: 1930,
+        [RatingSystem.Chesscom]: 2165,
+        [RatingSystem.Lichess]: 2225,
+        [RatingSystem.Ecf]: 1955,
+        [RatingSystem.Cfc]: 1925,
+        [RatingSystem.Dwz]: 1815,
+        [RatingSystem.Acf]: 1900,
+        [RatingSystem.Knsb]: 1910,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '1900-2000': {
+        [RatingSystem.Fide]: 2000,
+        [RatingSystem.Uscf]: 2055,
+        [RatingSystem.Chesscom]: 2275,
+        [RatingSystem.Lichess]: 2310,
+        [RatingSystem.Ecf]: 2065,
+        [RatingSystem.Cfc]: 2060,
+        [RatingSystem.Dwz]: 1940,
+        [RatingSystem.Acf]: 2000,
+        [RatingSystem.Knsb]: 2000,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '2000-2100': {
+        [RatingSystem.Fide]: 2100,
+        [RatingSystem.Uscf]: 2185,
+        [RatingSystem.Chesscom]: 2360,
+        [RatingSystem.Lichess]: 2370,
+        [RatingSystem.Ecf]: 2165,
+        [RatingSystem.Cfc]: 2185,
+        [RatingSystem.Dwz]: 2070,
+        [RatingSystem.Acf]: 2105,
+        [RatingSystem.Knsb]: 2100,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '2100-2200': {
+        [RatingSystem.Fide]: 2200,
+        [RatingSystem.Uscf]: 2290,
+        [RatingSystem.Chesscom]: 2425,
+        [RatingSystem.Lichess]: 2410,
+        [RatingSystem.Ecf]: 2260,
+        [RatingSystem.Cfc]: 2290,
+        [RatingSystem.Dwz]: 2185,
+        [RatingSystem.Acf]: 2215,
+        [RatingSystem.Knsb]: 2200,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '2200-2300': {
+        [RatingSystem.Fide]: 2300,
+        [RatingSystem.Uscf]: 2395,
+        [RatingSystem.Chesscom]: 2485,
+        [RatingSystem.Lichess]: 2440,
+        [RatingSystem.Ecf]: 2360,
+        [RatingSystem.Cfc]: 2395,
+        [RatingSystem.Dwz]: 2285,
+        [RatingSystem.Acf]: 2330,
+        [RatingSystem.Knsb]: 2300,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+    '2300-2400': {
+        [RatingSystem.Fide]: 2400,
+        [RatingSystem.Uscf]: 2500,
+        [RatingSystem.Chesscom]: 2550,
+        [RatingSystem.Lichess]: 2470,
+        [RatingSystem.Ecf]: 2460,
+        [RatingSystem.Cfc]: 2500,
+        [RatingSystem.Dwz]: 2385,
+        [RatingSystem.Acf]: 2450,
+        [RatingSystem.Knsb]: 2400,
+        [RatingSystem.Custom]: -1,
+        [RatingSystem.Custom2]: -1,
+        [RatingSystem.Custom3]: -1,
+    },
+};
+
+export function getRatingBoundary(
+    cohort: string,
+    ratingSystem: RatingSystem,
+    boundaries: Record<string, Record<RatingSystem, number>> = ratingBoundaries,
+): number | undefined {
+    const cohortBoundaries = boundaries[cohort];
+    if (!cohortBoundaries) {
+        return undefined;
+    }
+
+    const boundary = cohortBoundaries[ratingSystem];
+    if (boundary <= 0) {
+        return undefined;
+    }
+    return boundary;
+}
+
+/**
+ * Returns true if the given rating system is a custom system.
+ * @param ratingSystem The rating system to check.
+ */
+export function isCustom(ratingSystem: RatingSystem | string | undefined): boolean {
+    return (
+        ratingSystem === RatingSystem.Custom ||
+        ratingSystem === RatingSystem.Custom2 ||
+        ratingSystem === RatingSystem.Custom3
+    );
+}
+
+/**
+ * Returns the minimum rating for the given cohort and rating system.
+ * @param cohort The cohort to get the minimum rating for.
+ * @param ratingSystem The rating system to get the minimum rating for.
+ * @returns The minimum rating for the given cohort and rating system.
+ */
+export function getMinRatingBoundary(
+    cohort: string,
+    ratingSystem: RatingSystem,
+    boundaries: Record<string, Record<RatingSystem, number>> = ratingBoundaries,
+): number {
+    const cohortIdx = dojoCohorts.findIndex((c) => c === cohort);
+    if (cohortIdx <= 0) {
+        return 0;
+    }
+    return getRatingBoundary(dojoCohorts[cohortIdx - 1], ratingSystem, boundaries) || 0;
+}
+
+/**
+ * Returns the given cohort range as an array of 2 numbers. Ex: 0-1500
+ * would return [0, 1500]. For ranges like 2000+, the max cohort is set to Infinity
+ * (IE: [2000, Infinity]). If range is not provided or the min cohort is NaN, [-1, -1]
+ * is returned.
+ * @param range The cohort range to convert.
+ * @returns The min and max cohort as numbers.
+ */
+export function getCohortRangeInt(range?: string): [number, number] {
+    if (!range) {
+        return [-1, -1];
+    }
+
+    const minCohort = parseInt(range);
+    if (isNaN(minCohort)) {
+        return [-1, -1];
+    }
+
+    let maxCohort =
+        range.split('-').length > 1 ? parseInt(range.split('-')[1]) : Infinity;
+    if (isNaN(maxCohort)) {
+        maxCohort = Infinity;
+    }
+
+    return [minCohort, maxCohort];
+}
+
+export function getCohortForGivenRating(targetRating: number, ratingSystem: RatingSystem): string | undefined {
+
+    if(targetRating > ratingBoundaries['2300-2400'][ratingSystem]){
+        return `2300-2400`;
+    }
+
+    for (const cohort of dojoCohorts) {
+        console.log(`Checking cohort: ${cohort}`);
+        console.log(`Rating boundary for ${cohort}:`, ratingBoundaries[cohort]?.[ratingSystem]);
+
+        if (ratingBoundaries[cohort] && ratingBoundaries[cohort][ratingSystem] >= targetRating) {
+            console.log(`Found cohort: ${cohort}`);
+            return cohort;
+        }
+    }
+
+    console.log(`No cohort found for targetRating: ${targetRating}`);
+    return undefined;
+}
+
+export function getNormalizedRating(
+    rating: number,
+    ratingSystem: RatingSystem,
+    boundaries: Record<string, Record<RatingSystem, number>> = ratingBoundaries,
+): number {
+    if (isCustom(ratingSystem)) {
+        return -1;
+    }
+
+    for (const cohort of dojoCohorts) {
+        const x2 = getRatingBoundary(cohort, ratingSystem, boundaries);
+        if (!x2) {
+            continue;
+        }
+
+        if (x2 >= rating) {
+            const x1 = getMinRatingBoundary(cohort, ratingSystem, boundaries);
+
+            let [y1, y2] = getCohortRangeInt(cohort);
+
+            if (y1 === -1) {
+                y1 = 0;
+            }
+            if (y2 === -1) {
+                y2 = 0;
+            }
+
+            const result = ((y2 - y1) / (x2 - x1)) * (rating - x1) + y1;
+            return Math.round(result * 10) / 10;
+        }
+    }
+
+    // We are in the 2400+ cohort if we make it here, so we just extrapolate from the 2300-2400 line
+    const x1 = getMinRatingBoundary('2300-2400', ratingSystem, boundaries);
+    const x2 = getRatingBoundary('2300-2400', ratingSystem, boundaries) || 0;
+    const y1 = 2300;
+    const y2 = 2400;
+    const result = ((y2 - y1) / (x2 - x1)) * (rating - x1) + y1;
+    return Math.round(result * 10) / 10;
+}
