0e4b44db9d81307734d4ab5a912428f51f083534
Jack Stenglein <jackstenglein@gmail.com>
2025-05-31 17:38:13 -0500

    feat(#1505): add breadcrumbs to all uploads section

--- frontend/src/components/profile/directories/DirectoryCache.tsx
+++ frontend/src/components/profile/directories/DirectoryCache.tsx
@@ -252,9 +252,26 @@ export function useBreadcrumbs(owner: string, id: string, sharedOwner?: string) 
         }
     }
 
+    if (id === ALL_MY_UPLOADS_DIRECTORY_ID && !result.length) {
+        result.push(
+            {
+                name: 'All Uploads',
+                owner,
+                id: ALL_MY_UPLOADS_DIRECTORY_ID,
+                parent: HOME_DIRECTORY_ID,
+            },
+            {
+                name: 'Home',
+                owner,
+                id: HOME_DIRECTORY_ID,
+                parent: uuidNil,
+            },
+        );
+    }
+
     const setBreadcrumbs = cache.setBreadcrumbs;
     useEffect(() => {
-        if (id === uuidNil || request.isSent()) {
+        if (id === uuidNil || id === ALL_MY_UPLOADS_DIRECTORY_ID || request.isSent()) {
             return;
         }
 
