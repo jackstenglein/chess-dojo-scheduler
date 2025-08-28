0d98124b7850ddd1003139cfe14d20501803fe68
JosteinMunz <jostein.munz@kantega.no>
2025-08-28 15:53:00 +0200

    Feat(#1722) Old activity logs should display year

--- frontend/src/components/time/time.ts
+++ frontend/src/components/time/time.ts
@@ -1,1 +1,6 @@
 export const ONE_WEEK_IN_MS = 1000 * 60 * 60 * 24 * 7;
+export const ONE_YEAR_IN_MS = 1000 * 60 * 60 * 24 * 365;
+
+export const dateOlderThanAYear = (date: Date): boolean => {
+    return (new Date().getTime() - date.getTime()) > ONE_YEAR_IN_MS;
+}
