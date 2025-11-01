bbd1f135ce24a34c9f622c91e6fe612d9f8ec199
Jack Stenglein <jackstenglein@gmail.com>
2025-11-01 07:59:49 -0500

    fix(puzzles): move checkmate puzzles to new URL

---
+++ frontend/src/app/(scoreboard)/puzzles/checkmate/page.tsx
@@ -0,0 +1,8 @@
+import { CheckmatePuzzlePage } from '@/components/puzzles/checkmate/CheckmatePuzzlePage';
+
+/**
+ * Renders the Puzzles > Checkmate page.
+ */
+export default function CheckmatePage() {
+    return <CheckmatePuzzlePage />;
+}
