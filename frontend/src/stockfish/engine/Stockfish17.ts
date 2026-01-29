1e039e517ee6d763a9a76c3a04ccbd5f30e8fb63
Jack Stenglein <jackstenglein@gmail.com>
2026-01-29 16:41:02 -0600

    fix: format frontend files

--- frontend/src/stockfish/engine/Stockfish17.ts
+++ frontend/src/stockfish/engine/Stockfish17.ts
@@ -77,7 +77,7 @@ export class Stockfish17 extends UciEngine {
 
 export const sharedWasmMemory = (lo: number, hi = 32767): WebAssembly.Memory => {
     let shrink = 4; // 32767 -> 24576 -> 16384 -> 12288 -> 8192 -> 6144 -> etc
-     
+
     while (true) {
         try {
             return new WebAssembly.Memory({ shared: true, initial: lo, maximum: hi });
