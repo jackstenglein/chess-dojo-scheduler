e4ca08c677db7f2fe149ca957dfbaaa67c02b574
Jack Stenglein <jackstenglein@gmail.com>
2026-02-10 21:04:56 -0600

    feat(blog): allow admins to create/publish blog posts through editor

---
+++ frontend/src/app/(blog)/blog/[...id]/page.tsx
@@ -0,0 +1,50 @@
+import { getPublicBlog } from '@/api/blogApi';
+import { logger } from '@/logging/logger';
+import { Stack } from '@mui/material';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { Container } from '../common/Container';
+import { Footer } from '../common/Footer';
+import { Header } from '../common/Header';
+import { BlogMarkdown } from '../common/MarkdownEditor';
+
+const BLOG_OWNER = 'chessdojo';
+
+async function fetchBlog(id: string) {
+    const response = await getPublicBlog({ owner: BLOG_OWNER, id });
+    return response.data;
+}
+
+export async function generateMetadata({ params }: PageProps<'/blog/[...id]'>): Promise<Metadata> {
+    const { id: idSegments } = await params;
+    const id = idSegments.join('/');
+    const blog = await fetchBlog(id);
+    if (!blog) {
+        return { title: 'Not Found' };
+    }
+    return {
+        title: blog.title,
+        description: blog.subtitle,
+    };
+}
+
+export default async function BlogPage({ params }: PageProps<'/blog/[...id]'>) {
+    const { id: idSegments } = await params;
+    const id = idSegments.join('/');
+    logger.debug?.(`Fetching blog post: ${id}`);
+    const blog = await fetchBlog(id);
+
+    if (!blog) {
+        notFound();
+    }
+
+    return (
+        <Container>
+            <Header title={blog.title} subtitle={blog.subtitle} />
+            <Stack mt={3}>
+                <BlogMarkdown>{blog.content}</BlogMarkdown>
+                <Footer utmCampaign={`blog-${id}`} />
+            </Stack>
+        </Container>
+    );
+}
